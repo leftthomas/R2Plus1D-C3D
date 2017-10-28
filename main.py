579a3dd1a97f43f5a327d9498e02c51f1d55ac86
leftthomas <leftthomas@qq.com>
2017-10-28 20:56:30 +0800

    modified the tip

--- main.py
+++ main.py
@@ -10,34 +10,35 @@ from torch.utils.data import DataLoader
 import model
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-# train_data = datasets.MNIST(root='data/MNIST', train=True, transform=transforms.Compose(
-#     [transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize]), download=True)
+# normalize = transforms.Normalize(mean=[0.1307], std=[0.3081])
+# train_data = datasets.MNIST(root='data/MNIST', train=False, transform=transforms.Compose(
+#     [transforms.ToTensor(), normalize]), download=True)
 # test_data = datasets.MNIST(root='data/MNIST', train=False, transform=transforms.Compose(
-#     [transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize]), download=False)
-# train_data = datasets.CIFAR10(root='data/CIFAR10', train=True, transform=transforms.Compose(
-#     [transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize]), download=False)
-# test_data = datasets.CIFAR10(root='data/CIFAR10', train=False, transform=transforms.Compose(
-#     [transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize]), download=False)
+#     [transforms.ToTensor(), normalize]), download=False)
+train_data = datasets.CIFAR10(root='data/CIFAR10', train=True, transform=transforms.Compose(
+    [transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize]), download=False)
+test_data = datasets.CIFAR10(root='data/CIFAR10', train=False, transform=transforms.Compose(
+    [transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize]), download=False)
 # train_data = datasets.CIFAR100(root='data/CIFAR100', train=True, transform=transforms.Compose(
 #     [transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize]), download=False)
 # test_data = datasets.CIFAR100(root='data/CIFAR100', train=False, transform=transforms.Compose(
 #     [transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize]), download=False)
-train_data = datasets.STL10(root='data/STL10', split='train', transform=transforms.Compose(
-    [transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize]), download=False)
-test_data = datasets.STL10(root='data/STL10', split='test', transform=transforms.Compose(
-    [transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize]), download=False)
+# train_data = datasets.STL10(root='data/STL10', split='train', transform=transforms.Compose(
+#     [transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize]), download=False)
+# test_data = datasets.STL10(root='data/STL10', split='test', transform=transforms.Compose(
+#     [transforms.RandomHorizontalFlip(), transforms.ToTensor(), normalize]), download=False)
 
-train_loader = DataLoader(dataset=train_data, batch_size=32, shuffle=True)
-test_loader = DataLoader(dataset=test_data, batch_size=32, shuffle=True)
+train_loader = DataLoader(dataset=train_data, batch_size=16, shuffle=True)
+test_loader = DataLoader(dataset=test_data, batch_size=16, shuffle=True)
 
 net = model.Net()
 if torch.cuda.is_available():
     net.cuda()
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(params=net.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
+optimizer = optim.SGD(params=net.parameters(), lr=0.01, momentum=0.5, weight_decay=1e-4)
 
-scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[5, 10, 15, 22], gamma=0.1)
+scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[15, 22], gamma=0.1)
 for epoch in range(1, 31):
     net.train()
     scheduler.step()
@@ -65,5 +66,5 @@ for epoch in range(1, 31):
         pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
-    print('\nTest set: Accuracy: {}/{} ({:.0f}%)\n'.format(correct, len(test_loader.dataset),
+    print('\nTest set: Accuracy: {}/{} ({:.2f}%)\n'.format(correct, len(test_loader.dataset),
                                                            100. * correct / len(test_loader.dataset)))
--- model.py
+++ model.py
@@ -1,64 +1,25 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-channel_rate = 96
-
 
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=channel_rate, kernel_size=3, padding=1, dilation=1)
-        self.bn1 = nn.BatchNorm2d(num_features=channel_rate)
-
-        self.conv2 = nn.Conv2d(in_channels=channel_rate, out_channels=2 * channel_rate, kernel_size=3, padding=2,
-                               dilation=2)
-        self.bn2 = nn.BatchNorm2d(num_features=2 * channel_rate)
-
-        self.conv3 = nn.Conv2d(in_channels=2 * channel_rate, out_channels=3 * channel_rate, kernel_size=3, padding=3,
-                               dilation=3)
-        self.bn3 = nn.BatchNorm2d(num_features=3 * channel_rate)
-
-        self.conv4 = nn.Conv2d(in_channels=3 * channel_rate, out_channels=3 * channel_rate, kernel_size=5, padding=6,
-                               dilation=3)
-        self.bn4 = nn.BatchNorm2d(num_features=3 * channel_rate)
-
-        self.conv5 = nn.Conv2d(in_channels=3 * channel_rate, out_channels=2 * channel_rate, kernel_size=3, padding=3,
-                               dilation=3)
-        self.bn5 = nn.BatchNorm2d(num_features=2 * channel_rate)
-
-        self.conv6 = nn.Conv2d(in_channels=2 * channel_rate, out_channels=channel_rate, kernel_size=3, padding=2,
-                               dilation=2)
-        self.bn6 = nn.BatchNorm2d(num_features=channel_rate)
-
-        self.conv7 = nn.Conv2d(in_channels=channel_rate, out_channels=3, kernel_size=3, padding=1, dilation=1)
-        self.bn7 = nn.BatchNorm2d(num_features=3)
-
-        self.fc1 = nn.Linear(in_features=channel_rate * channel_rate * 3, out_features=48 * 48 * 2)
-        self.fc2 = nn.Linear(in_features=48 * 48 * 2, out_features=24 * 24 * 1)
-        self.fc3 = nn.Linear(in_features=24 * 24 * 1, out_features=10)
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=3)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=4)
+        self.conv3 = nn.Conv2d(20, 30, kernel_size=3)
+        self.conv3_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(120, 50)
+        self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
-        x = F.leaky_relu(self.bn1(self.conv1(x)))
-        # print(x.data.size())
-        x = F.leaky_relu(self.bn2(self.conv2(x)))
-        # print(x.data.size())
-        x = F.leaky_relu(self.bn3(self.conv3(x)))
-        # print(x.data.size())
-        x = F.leaky_relu(self.bn4(self.conv4(x)))
-        # print(x.data.size())
-        x = F.leaky_relu(self.bn5(self.conv5(x)))
-        # print(x.data.size())
-        x = F.leaky_relu(self.bn6(self.conv6(x)))
-        # print(x.data.size())
-        x = F.leaky_relu(self.bn7(self.conv7(x)))
-        # print(x.data.size())
-
-        x = x.view(-1, channel_rate * channel_rate * 3)
-        x = self.fc1(x)
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(F.max_pool2d(self.conv3_drop(self.conv3(x)), 2))
+        x = x.view(-1, 120)
+        x = F.relu(self.fc1(x))
         x = F.dropout(x)
         x = self.fc2(x)
-        x = F.dropout(x)
-        x = self.fc3(x)
         return x
 
 
