0718062e841595fcc81e2b7b6e77cfe3a839b289
leftthomas <leftthomas@qq.com>
2017-11-11 03:25:08 +0800

    fix bug

--- capsnet.py
+++ capsnet.py
@@ -12,7 +12,7 @@ class CapsuleNet(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=256, kernel_size=9, stride=1)
         self.primary_capsules = CapsuleLayer(num_capsules=8, num_route_nodes=-1, in_channels=256, out_channels=64,
                                              kernel_size=9, stride=2)
-        self.class_capsules = CapsuleLayer(num_capsules=config.NUM_CLASSES, num_route_nodes=32 * 4 * 4,
+        self.class_capsules = CapsuleLayer(num_capsules=config.NUM_CLASSES, num_route_nodes=64 * 4 * 4,
                                            in_channels=8,
                                            out_channels=16)
 
