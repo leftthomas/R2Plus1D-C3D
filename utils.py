bdc6583c0f9848ea8a4d607140aa202f92bcd9af
leftthomas <leftthomas@qq.com>
2017-11-13 10:15:29 +0800

    change scale

--- utils.py
+++ utils.py
@@ -22,10 +22,10 @@ def augmentation(x, max_shift=2):
 
 
 def get_iterator(mode):
-    tr = transforms.Compose([transforms.RandomCrop(size=24), transforms.ToTensor()])
+    tr = transforms.Compose([transforms.Scale(size=24), transforms.ToTensor()])
     data = CIFAR10(root='data/CIFAR10', train=mode, transform=tr, download=True)
-    # dataset = CIFAR100(root='data/CIFAR100', train=mode, transform=transforms.ToTensor(), download=True)
-    # dataset = STL10(root='data/STL10', split='train', transform=transforms.ToTensor(), download=True)
+    # dataset = CIFAR100(root='data/CIFAR100', train=mode, transform=tr, download=True)
+    # dataset = STL10(root='data/STL10', split='train', transform=tr, download=True)
 
     return DataLoader(dataset=data, batch_size=config.BATCH_SIZE, shuffle=mode, num_workers=4)
 
