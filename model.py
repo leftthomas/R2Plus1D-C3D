2ffdc8719a0dc199d62b8b8f56d551fb9f8f2ca7
leftthomas <leftthomas@qq.com>
2017-11-23 13:46:08 +0800

    fix class bug

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ def squash(tensor, dim=1):
 
 
 class SquashCapsuleNet(nn.Module):
-    def __init__(self, in_channels, classes):
+    def __init__(self, in_channels, num_class):
         super(SquashCapsuleNet, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=128, kernel_size=5, stride=1, padding=2)
         self.conv2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
@@ -18,9 +18,10 @@ class SquashCapsuleNet(nn.Module):
         self.conv4 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1, groups=16)
         self.conv5 = nn.Conv2d(in_channels=512, out_channels=256, kernel_size=3, stride=1, padding=1, groups=8)
         self.conv6 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, stride=1, padding=1, groups=4)
-        self.conv7 = nn.Conv2d(in_channels=128, out_channels=in_channels, kernel_size=5, stride=1, padding=2)
+        self.conv7 = nn.Conv2d(in_channels=128, out_channels=in_channels * num_class, kernel_size=5, stride=1,
+                               padding=2)
         self.lrelu = nn.LeakyReLU(0.2, inplace=True)
-        self.classes = classes
+        self.num_class = num_class
 
     def forward(self, x):
         x = self.lrelu(self.conv1(x))
@@ -37,7 +38,7 @@ class SquashCapsuleNet(nn.Module):
         x = torch.cat([squash(capsule) for capsule in torch.chunk(x, chunks=4, dim=1)], dim=1)
 
         x = self.conv7(x)
-        x = x.view(x.size(0), self.classes, -1).norm(dim=-1)
+        x = x.view(x.size(0), self.num_class, -1).norm(dim=-1)
         return F.sigmoid(x)
 
 
@@ -45,5 +46,5 @@ if __name__ == "__main__":
     a = torch.FloatTensor([[0, 1, 2], [3, 4, 5]])
     b = squash(a)
     print(b)
-    d = SquashCapsuleNet(in_channels=1, classes=10)
+    d = SquashCapsuleNet(in_channels=1, num_class=10)
     print(d)
