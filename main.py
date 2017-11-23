fix AverageValueMeter bug

main.py
@@ -10,6 +10,7 @@ from torchnet.logger import VisdomPlotLogger, VisdomLogger
 from tqdm import tqdm
 
 import utils
+from averagevaluemeter import AverageValueMeter
 from model import SquashCapsuleNet
 
 
@@ -51,20 +52,21 @@ def on_start_epoch(state):
 
 def on_end_epoch(state):
     print('[Epoch %d] Training Loss: %.4f (Accuracy: %.2f%%)' % (
-        state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0]))
+        state['epoch'], meter_loss.value(), meter_accuracy.value()[0]))
 
-    train_loss_logger.log(state['epoch'], meter_loss.value()[0])
+    train_loss_logger.log(state['epoch'], meter_loss.value())
     train_accuracy_logger.log(state['epoch'], meter_accuracy.value()[0])
 
     reset_meters()
 
     engine.test(processor, utils.get_iterator(False, DATA_TYPE))
-    test_loss_logger.log(state['epoch'], meter_loss.value()[0])
+
+    test_loss_logger.log(state['epoch'], meter_loss.value())
     test_accuracy_logger.log(state['epoch'], meter_accuracy.value()[0])
     confusion_logger.log(confusion_meter.value())
 
     print('[Epoch %d] Testing Loss: %.4f (Accuracy: %.2f%%)' % (
-        state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0]))
+        state['epoch'], meter_loss.value(), meter_accuracy.value()[0]))
 
     torch.save(model.state_dict(), 'epochs/epoch_%d.pt' % state['epoch'])
 
@@ -99,7 +101,7 @@ if __name__ == '__main__':
     optimizer = Adam(model.parameters())
 
     engine = Engine()
-    meter_loss = tnt.meter.AverageValueMeter()
+    meter_loss = AverageValueMeter()
     meter_accuracy = tnt.meter.ClassErrorMeter(accuracy=True)
     confusion_meter = tnt.meter.ConfusionMeter(CLASSES, normalized=True)
 