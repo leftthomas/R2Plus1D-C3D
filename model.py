98facc0e4459c7d73adb19b05d40292d4ae82a89
leftthomas <leftthomas@qq.com>
2017-11-25 12:47:08 +0800

    remove lrelu with bn

--- model.py
+++ model.py
@@ -16,10 +16,13 @@ class SquashCapsuleNet(nn.Module):
         self.conv2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=5, stride=1, padding=4, dilation=2)
         self.conv3 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=3, dilation=3,
                                groups=4)
+        self.bn3 = nn.BatchNorm2d(num_features=512)
         self.conv4 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=3, dilation=3,
                                groups=8)
+        self.bn4 = nn.BatchNorm2d(num_features=512)
         self.conv5 = nn.Conv2d(in_channels=512, out_channels=256, kernel_size=3, stride=1, padding=3, dilation=3,
                                groups=4)
+        self.bn5 = nn.BatchNorm2d(num_features=256)
         self.conv6 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=5, stride=1, padding=4, dilation=2)
         self.conv7 = nn.Conv2d(in_channels=128, out_channels=in_channels * num_class, kernel_size=7, stride=1,
                                padding=3)
@@ -30,12 +33,12 @@ class SquashCapsuleNet(nn.Module):
         x = self.lrelu(self.conv1(x))
         x = self.lrelu(self.conv2(x))
 
-        x = self.lrelu(self.conv3(x))
         # capsules squash
+        x = self.bn3(self.conv3(x))
         x = torch.cat([squash(capsule) for capsule in torch.chunk(x, chunks=4, dim=1)], dim=1)
-        x = self.lrelu(self.conv4(x))
+        x = self.bn4(self.conv4(x))
         x = torch.cat([squash(capsule) for capsule in torch.chunk(x, chunks=8, dim=1)], dim=1)
-        x = self.lrelu(self.conv5(x))
+        x = self.bn5(self.conv5(x))
         x = torch.cat([squash(capsule) for capsule in torch.chunk(x, chunks=4, dim=1)], dim=1)
 
         x = self.lrelu(self.conv6(x))
