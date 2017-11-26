6149553cd3732e3aa904d7eed0e992f35b0bef3b
leftthomas <leftthomas@qq.com>
2017-11-26 13:55:49 +0800

    change model

--- model.py
+++ model.py
@@ -11,43 +11,34 @@ def squash(tensor, dim=1):
 class SquashCapsuleNet(nn.Module):
     def __init__(self, in_channels, num_class):
         super(SquashCapsuleNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=128, kernel_size=7, stride=1, padding=3,
-                               dilation=1,
-                               groups=1)
-        self.conv2 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=5, stride=1, padding=4, dilation=2,
-                               groups=4)
-        self.conv3 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=3, dilation=3,
-                               groups=16)
-        self.conv4 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=3, dilation=3,
-                               groups=16)
-        self.conv5 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=3, dilation=3,
-                               groups=16)
-        self.conv6 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=5, stride=1, padding=4, dilation=2,
-                               groups=4)
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=64, kernel_size=7, stride=1, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, stride=1, padding=2)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=2, padding=1)
+        self.bn4 = nn.BatchNorm2d(512)
+        self.conv5 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=2, padding=1)
+        self.bn5 = nn.BatchNorm2d(512)
+        self.lrelu = nn.LeakyReLU(0.2)
 
         self.adaavgpool = nn.AdaptiveAvgPool2d(1)
-        self.classifier = nn.Sequential(nn.Conv2d(128, 256, kernel_size=1), nn.LeakyReLU(0.2),
-                                        nn.Conv2d(256, num_class, kernel_size=1))
+        self.classifier = nn.Linear(512, num_class)
 
     def forward(self, x):
         batch_size = x.size(0)
         # capsules squash
-        x = self.conv1(x)
+        x = self.lrelu(self.bn1(self.conv1(x)))
+        x = self.lrelu(self.bn2(self.conv2(x)))
+        x = self.lrelu(self.bn3(self.conv3(x)))
+        x = self.lrelu(self.bn4(self.conv4(x)))
+        x = self.lrelu(self.bn5(self.conv5(x)))
         x = torch.cat([squash(capsule) for capsule in torch.chunk(x, chunks=1, dim=1)], dim=1)
-        x = self.conv2(x)
-        x = torch.cat([squash(capsule) for capsule in torch.chunk(x, chunks=4, dim=1)], dim=1)
-        x = self.conv3(x)
-        x = torch.cat([squash(capsule) for capsule in torch.chunk(x, chunks=16, dim=1)], dim=1)
-        x = self.conv4(x)
-        x = torch.cat([squash(capsule) for capsule in torch.chunk(x, chunks=16, dim=1)], dim=1)
-        x = self.conv5(x)
-        x = torch.cat([squash(capsule) for capsule in torch.chunk(x, chunks=16, dim=1)], dim=1)
-        x = self.conv6(x)
-        x = torch.cat([squash(capsule) for capsule in torch.chunk(x, chunks=4, dim=1)], dim=1)
 
         x = self.adaavgpool(x)
-        x = self.classifier(x)
         x = x.view(batch_size, -1)
+        x = self.classifier(x)
         return x
 
 
