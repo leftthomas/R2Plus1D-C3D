0136197470877dcc1bba9872b9938caf5d75205f
leftthomas <leftthomas@qq.com>
2017-11-27 13:50:18 +0800

    using data augmentation

--- utils.py
+++ utils.py
@@ -7,12 +7,32 @@ from torchvision.datasets.svhn import SVHN
 
 
 def get_iterator(mode, data_type):
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
     if data_type == 'MNIST':
         data = MNIST(root='data/MNIST', train=mode, transform=transforms.ToTensor(), download=True)
+
     elif data_type == 'CIFAR10':
-        data = CIFAR10(root='data/CIFAR10', train=mode, transform=transforms.ToTensor(), download=True)
+        if mode:
+            data = CIFAR10(root='data/CIFAR10', train=mode, transform=transform_train, download=True)
+        else:
+            data = CIFAR10(root='data/CIFAR10', train=mode, transform=transform_test, download=True)
+
     elif data_type == 'CIFAR100':
-        data = CIFAR100(root='data/CIFAR100', train=mode, transform=transforms.ToTensor(), download=True)
+        if mode:
+            data = CIFAR100(root='data/CIFAR100', train=mode, transform=transform_train, download=True)
+        else:
+            data = CIFAR100(root='data/CIFAR100', train=mode, transform=transform_test, download=True)
+
     elif data_type == 'STL10':
         if mode:
             data = STL10(root='data/STL10', split='train', transform=transforms.ToTensor(), download=True)
