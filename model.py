fbc206d5b768fb4b79de8a288ec54559dc07152c
leftthomas <leftthomas@qq.com>
2017-11-30 15:15:48 +0800

    fix bugs

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ class SquashCapsuleNet(nn.Module):
     def __init__(self, in_channels, num_class, data_type):
         super(SquashCapsuleNet, self).__init__()
         self.features = self.make_layers(in_channels, config[data_type])
-        self.classifier = CapsuleLinear(in_capsules=4 * 4 * 512 // 8, out_capsules=num_class, in_length=8,
+        self.classifier = CapsuleLinear(in_capsules=4 * 4 * 256 // 8, out_capsules=num_class, in_length=8,
                                         out_length=16)
 
     def forward(self, x):
