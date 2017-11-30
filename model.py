489176b9da833e7c67e5a307711f9d3b7cdd1a20
leftthomas <leftthomas@qq.com>
2017-11-30 15:40:33 +0800

    change model

--- model.py
+++ model.py
@@ -4,11 +4,11 @@ from torch import nn
 from capsulelayer import CapsuleLinear, CapsuleConv2d
 
 config = {
-    'MNIST': [32, '32D', 64, '64D', 128, '128D'],
-    'CIFAR10': [32, '32D', 64, '64D', 128, '128D'],
-    'CIFAR100': [32, '32D', 64, '64D', 128, '128D'],
-    'STL10': [32, '32D', 64, '64D', 128, '128D'],
-    'SVHN': [32, '32D', 64, '64D', 128, '128D'],
+    'MNIST': [16, '16D', 32, '32D', 64, '64D'],
+    'CIFAR10': [16, '16D', 32, '32D', 64, '64D'],
+    'CIFAR100': [16, '16D', 32, '32D', 64, '64D'],
+    'STL10': [16, '16D', 32, '32D', 64, '64D'],
+    'SVHN': [16, '16D', 32, '32D', 64, '64D'],
 }
 
 
@@ -16,7 +16,7 @@ class SquashCapsuleNet(nn.Module):
     def __init__(self, in_channels, num_class, data_type):
         super(SquashCapsuleNet, self).__init__()
         self.features = self.make_layers(in_channels, config[data_type])
-        self.classifier = CapsuleLinear(in_capsules=4 * 4 * 128 // 8, out_capsules=num_class, in_length=8,
+        self.classifier = CapsuleLinear(in_capsules=2 * 2 * 64 // 8, out_capsules=num_class, in_length=8,
                                         out_length=16)
 
     def forward(self, x):
@@ -30,10 +30,10 @@ class SquashCapsuleNet(nn.Module):
     @staticmethod
     def make_layers(in_channels, cfg):
         layers = []
-        layers += [nn.Conv2d(in_channels, 32, kernel_size=3, padding=1, stride=1),
-                   nn.BatchNorm2d(32),
+        layers += [nn.Conv2d(in_channels, 16, kernel_size=3, padding=1, stride=1),
+                   nn.BatchNorm2d(16),
                    nn.ReLU(inplace=True)]
-        in_channels = 32
+        in_channels = 16
         for x in cfg:
             if type(x) == str:
                 x = int(x.replace('D', ''))
@@ -41,7 +41,7 @@ class SquashCapsuleNet(nn.Module):
             else:
                 layers += [CapsuleConv2d(in_channels, x, kernel_size=3, in_length=8, out_length=8, padding=1)]
             in_channels = x
-        layers += [nn.AdaptiveAvgPool2d(4)]
+        layers += [nn.AdaptiveAvgPool2d(2)]
         return nn.Sequential(*layers)
 
 
