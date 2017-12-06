3cb18afc353e9c68b8eb9ec67967c65dee785197
leftthomas <leftthomas@qq.com>
2017-12-06 20:11:46 +0800

    fix bug

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def on_start_epoch(state):
 
 def on_end_epoch(state):
     print('[Epoch %d] Training Loss: %.4f (Accuracy: %.2f%%)' % (
-        state['epoch'], meter_loss.value(), meter_accuracy.value()[0]))
+        state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0]))
 
     train_loss_logger.log(state['epoch'], meter_loss.value())
     train_accuracy_logger.log(state['epoch'], meter_accuracy.value()[0])
@@ -60,12 +60,12 @@ def on_end_epoch(state):
 
     engine.test(processor, utils.get_iterator(False, DATA_TYPE, USING_DATA_AUGMENTATION))
 
-    test_loss_logger.log(state['epoch'], meter_loss.value())
+    test_loss_logger.log(state['epoch'], meter_loss.value()[0])
     test_accuracy_logger.log(state['epoch'], meter_accuracy.value()[0])
     confusion_logger.log(confusion_meter.value())
 
     print('[Epoch %d] Testing Loss: %.4f (Accuracy: %.2f%%)' % (
-        state['epoch'], meter_loss.value(), meter_accuracy.value()[0]))
+        state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0]))
 
     torch.save(model.state_dict(), 'epochs/epoch_%d.pt' % state['epoch'])
 
