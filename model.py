64ad27a893f78365f03cbc520d221b6860d40f6e
leftthomas <leftthomas@qq.com>
2017-12-10 19:43:11 +0800

    using mean to replace sum

--- model.py
+++ model.py
@@ -7,9 +7,9 @@ config = {
     # each ceil form: out_channels, out_length, (D)[means do CapsuleConv2d at stride=2]
     'MNIST': ['32-8', '32-8D', '64-16', '64-16D', '128-32', '128-32D'],
     'FashionMNIST': ['32-8', '32-8D', '64-16', '64-16D', '128-32', '128-32D'],
-    'SVHN': ['32-8', '32-8D', '64-16', '64-16D', '128-32', '128-32D', '256-64', '256-64D'],
-    'CIFAR10': ['32-8', '32-8D', '64-16', '64-16D', '128-32', '128-32D', '256-64', '256-64D'],
-    'CIFAR100': ['32-8', '32-8D', '64-16', '64-16D', '128-32', '128-32D', '256-64', '256-64D'],
+    'SVHN': ['32-8', '32-8D', '64-16', '64-16D', '128-32', '128-32D'],
+    'CIFAR10': ['32-8', '32-8D', '64-16', '64-16D', '128-32', '128-32D'],
+    'CIFAR100': ['32-8', '32-8D', '64-16', '64-16D', '128-32', '128-32D'],
     'STL10': ['32-8', '32-8D', '64-16', '64-16D', '128-32', '128-32D'],
 }
 
@@ -18,15 +18,15 @@ class SquashCapsuleNet(nn.Module):
     def __init__(self, in_channels, num_class, data_type):
         super(SquashCapsuleNet, self).__init__()
         self.features = self.make_layers(in_channels, config[data_type])
-        self.classifier = CapsuleLinear(in_capsules=2 * 2 * 256 // 64, out_capsules=num_class, in_length=64,
-                                        out_length=64)
+        self.classifier = CapsuleLinear(in_capsules=4 * 4 * 128 // 32, out_capsules=num_class, in_length=32,
+                                        out_length=32)
 
     def forward(self, x):
         out = self.features(x)
 
         out = out.view(*out.size()[:2], -1)
         out = out.transpose(-1, -2)
-        out = out.contiguous().view(out.size(0), -1, 64)
+        out = out.contiguous().view(out.size(0), -1, 32)
 
         out = self.classifier(out)
         classes = (out ** 2).sum(dim=-1) ** 0.5
