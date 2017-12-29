a174782955f326ff8813001d9f341561b028e487
leftthomas <leftthomas@qq.com>
2017-12-29 20:58:51 +0800

    add feature visualization

---
+++ feature_visualization.py
@@ -0,0 +1,218 @@
+import argparse
+
+import cv2
+import numpy as np
+import torch
+from torch.autograd import Function
+from torch.autograd import Variable
+from torchvision import models
+from torchvision import utils
+
+
+class FeatureExtractor:
+    """ Class for extracting activations and 
+    registering gradients from targetted intermediate layers """
+
+    def __init__(self, model, target_layers):
+        self.model = model
+        self.target_layers = target_layers
+        self.gradients = []
+
+    def save_gradient(self, grad):
+        self.gradients.append(grad)
+
+    def __call__(self, x):
+        outputs = []
+        self.gradients = []
+        for name, module in self.model._modules.items():
+            x = module(x)
+            if name in self.target_layers:
+                x.register_hook(self.save_gradient)
+                outputs += [x]
+        return outputs, x
+
+
+class ModelOutputs:
+    """ Class for making a forward pass, and getting:
+    1. The network output.
+    2. Activations from intermeddiate targetted layers.
+    3. Gradients from intermeddiate targetted layers. """
+
+    def __init__(self, model, target_layers):
+        self.model = model
+        self.feature_extractor = FeatureExtractor(self.model.features, target_layers)
+
+    def get_gradients(self):
+        return self.feature_extractor.gradients
+
+    def __call__(self, x):
+        target_activations, output = self.feature_extractor(x)
+        output = output.view(output.size(0), -1)
+        output = self.model.classifier(output)
+        return target_activations, output
+
+
+def preprocess_image(img):
+    means = [0.485, 0.456, 0.406]
+    stds = [0.229, 0.224, 0.225]
+
+    preprocessed_img = img.copy()[:, :, ::-1]
+    for j in range(3):
+        preprocessed_img[:, :, j] = preprocessed_img[:, :, j] - means[j]
+        preprocessed_img[:, :, j] = preprocessed_img[:, :, j] / stds[j]
+    preprocessed_img = np.ascontiguousarray(np.transpose(preprocessed_img, (2, 0, 1)))
+    preprocessed_img = torch.from_numpy(preprocessed_img)
+    preprocessed_img.unsqueeze_(0)
+    x = Variable(preprocessed_img, requires_grad=True)
+    return x
+
+
+def show_cam_on_image(img, mask):
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
+    heatmap = np.float32(heatmap) / 255
+    cam = heatmap + np.float32(img)
+    cam = cam / np.max(cam)
+    cv2.imwrite("cam.jpg", np.uint8(255 * cam))
+
+
+class GradCam:
+    def __init__(self, model, target_layer_names):
+        self.model = model
+        self.model.eval()
+        if torch.cuda.is_available():
+            self.model = model.cuda()
+
+        self.extractor = ModelOutputs(self.model, target_layer_names)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def __call__(self, x, index=None):
+        if torch.cuda.is_available():
+            features, output = self.extractor(x.cuda())
+        else:
+            features, output = self.extractor(x)
+
+        if index is None:
+            index = np.argmax(output.cpu().data.numpy())
+
+        one_hot = np.zeros((1, output.size()[-1]), dtype=np.float32)
+        one_hot[0][index] = 1
+        one_hot = Variable(torch.from_numpy(one_hot), requires_grad=True)
+        if torch.cuda.is_available():
+            one_hot = torch.sum(one_hot.cuda() * output)
+        else:
+            one_hot = torch.sum(one_hot * output)
+
+        self.model.features.zero_grad()
+        self.model.classifier.zero_grad()
+        one_hot.backward(retain_graph=True)
+
+        grads_val = self.extractor.get_gradients()[-1].cpu().data.numpy()
+
+        target = features[-1]
+        target = target.cpu().data.numpy()[0, :]
+
+        weights = np.mean(grads_val, axis=(2, 3))[0, :]
+        cam = np.ones(target.shape[1:], dtype=np.float32)
+
+        for j, w in enumerate(weights):
+            cam += w * target[j, :, :]
+
+        cam = np.maximum(cam, 0)
+        cam = cv2.resize(cam, (224, 224))
+        cam = cam - np.min(cam)
+        cam = cam / np.max(cam)
+        return cam
+
+
+class GuidedBackpropReLU(Function):
+
+    def forward(self, x, **kwargs):
+        positive_mask = (x > 0).type_as(x)
+        output = torch.addcmul(torch.zeros(x.size()).type_as(x), x, positive_mask)
+        self.save_for_backward(x, output)
+        return output
+
+    def backward(self, grad_output):
+        input, output = self.saved_tensors
+        positive_mask_1 = (input > 0).type_as(grad_output)
+        positive_mask_2 = (grad_output > 0).type_as(grad_output)
+        grad_input = torch.addcmul(torch.zeros(input.size()).type_as(input),
+                                   torch.addcmul(torch.zeros(input.size()).type_as(input), grad_output,
+                                                 positive_mask_1), positive_mask_2)
+        return grad_input
+
+
+class GuidedBackpropReLUModel:
+    def __init__(self, model):
+        self.model = model
+        self.model.eval()
+        if torch.cuda.is_available():
+            self.model = model.cuda()
+
+        # replace ReLU with GuidedBackpropReLU
+        for idx, module in self.model.features._modules.items():
+            if module.__class__.__name__ == 'ReLU':
+                self.model.features._modules[idx] = GuidedBackpropReLU()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def __call__(self, x, index=None):
+        if torch.cuda.is_available():
+            output = self.forward(x.cuda())
+        else:
+            output = self.forward(x)
+
+        if index is None:
+            index = np.argmax(output.cpu().data.numpy())
+
+        one_hot = np.zeros((1, output.size()[-1]), dtype=np.float32)
+        one_hot[0][index] = 1
+        one_hot = Variable(torch.from_numpy(one_hot), requires_grad=True)
+        if torch.cuda.is_available():
+            one_hot = torch.sum(one_hot.cuda() * output)
+        else:
+            one_hot = torch.sum(one_hot * output)
+
+        # self.model.features.zero_grad()
+        # self.model.classifier.zero_grad()
+        one_hot.backward(retain_graph=True)
+
+        output = x.grad.cpu().data.numpy()
+        output = output[0, :, :, :]
+
+        return output
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Feature Visualization')
+    parser.add_argument('--image_path', type=str, help='input image path')
+    parser.add_argument('--target_index', default=None, help='the index of scoring category')
+    opt = parser.parse_args()
+    IMAGE_PATH = opt.image_path
+    TARGET_INDEX = opt.target_index
+    net = models.vgg19(pretrained=True)
+
+    grad_cam = GradCam(model=net, target_layer_names=["35"])
+
+    img = cv2.imread(IMAGE_PATH, cv2.CAP_MODE_RGB)
+    img = np.float32(cv2.resize(img, (224, 224))) / 255
+    image = preprocess_image(img)
+
+    # If None, returns the map for the highest scoring category.
+    # Otherwise, targets the requested index.
+    mask = grad_cam(image, TARGET_INDEX)
+    show_cam_on_image(img, mask)
+
+    gb_model = GuidedBackpropReLUModel(model=net)
+    gb = gb_model(image, index=TARGET_INDEX)
+    utils.save_image(torch.from_numpy(gb), 'gb.jpg')
+
+    cam_mask = np.zeros(gb.shape)
+    for i in range(0, gb.shape[0]):
+        cam_mask[i, :, :] = mask
+
+    cam_gb = np.multiply(cam_mask, gb)
+    utils.save_image(torch.from_numpy(cam_gb), 'cam_gb.jpg')
