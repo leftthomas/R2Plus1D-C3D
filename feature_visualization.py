8da33e495f8c410c7b260d450480d15d092b2d54
leftthomas <leftthomas@qq.com>
2018-01-04 18:52:00 +0800

    simplified code

--- feature_visualization.py
+++ feature_visualization.py
@@ -9,35 +9,35 @@ from torchvision import models, transforms
 
 
 class GradCam:
-    def __init__(self, model, target_layer_names):
+    def __init__(self, model, target_layer_names, target_index):
         self.model = model.eval()
         if torch.cuda.is_available():
             self.model = model.cuda()
         self.target_layers = target_layer_names
+        self.target_index = target_index
+        self.features = []
         self.gradients = []
 
     def save_gradient(self, grad):
         self.gradients.append(grad)
 
-    def __call__(self, x, index=None):
+    def __call__(self, x):
+        # save the target layers' gradients and features, then get the classes scores
         if torch.cuda.is_available():
             x = x.cuda()
-
-        features = []
-        self.gradients = []
         for name, module in self.model.features.named_children():
             x = module(x)
             if name in self.target_layers:
                 x.register_hook(self.save_gradient)
-                features += [x]
-        output = x.view(x.size(0), -1)
-        output = self.model.classifier(output)
+                self.features += [x]
+        x = x.view(x.size(0), -1)
+        output = self.model.classifier(x)
 
-        if index is None:
-            index = np.argmax(output.cpu().data.numpy())
+        if self.target_index is None:
+            self.target_index = np.argmax(output.cpu().data.numpy())
 
         one_hot = torch.zeros(output.size())
-        one_hot[0][index] = 1
+        one_hot[0][self.target_index] = 1
         one_hot = Variable(one_hot)
         if torch.cuda.is_available():
             one_hot = torch.sum(one_hot.cuda() * output)
@@ -50,7 +50,7 @@ class GradCam:
 
         grads_val = self.gradients[-1].cpu().data.numpy()
 
-        target = features[-1]
+        target = self.features[-1]
         target = target.cpu().data.numpy()[0, :]
 
         weights = np.mean(grads_val, axis=(2, 3))[0, :]
@@ -60,7 +60,7 @@ class GradCam:
             cam += w * target[j, :, :]
 
         cam = np.maximum(cam, 0)
-        cam = cv2.resize(cam, IMAGE_SIZE)
+        cam = cv2.resize(cam, (224, 224))
         cam = cam - np.min(cam)
         cam = cam / np.max(cam)
         return cam
@@ -83,14 +83,13 @@ if __name__ == '__main__':
     TARGET_INDEX = opt.target_index
 
     image = Image.open(IMAGE_PATH)
-    IMAGE_SIZE = image.size
-    image = transforms.Resize(IMAGE_SIZE)(image)
+    image = transforms.Resize((224, 224))(image)
     image = transforms.ToTensor()(image)
     image = Variable(image.unsqueeze(dim=0))
 
     net = models.vgg19(pretrained=True)
-    grad_cam = GradCam(model=net, target_layer_names=['35'])
+    grad_cam = GradCam(net, ['35'], TARGET_INDEX)
 
     # If None, returns the map for the highest scoring category.
     # Otherwise, targets the requested index.
-    show_cam_on_image(image.squeeze(dim=0).data.numpy().transpose(1, 2, 0), grad_cam(image, TARGET_INDEX))
+    show_cam_on_image(image.squeeze(dim=0).data.numpy().transpose(1, 2, 0), grad_cam(image))
