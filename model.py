910f5f3bb5add4587971bd1f56eacbd717bb22f4
leftthomas <leftthomas@qq.com>
2018-01-05 21:09:08 +0800

    change CIFAR10CapsuleNet

--- model.py
+++ model.py
@@ -82,29 +82,29 @@ class CIFAR10CapsuleNet(nn.Module):
                           padding=1),
             CapsuleConv2d(in_channels=64, out_channels=128, kernel_size=3, in_length=8, out_length=8, stride=1,
                           padding=1),
-            CapsuleConv2d(in_channels=128, out_channels=128, kernel_size=3, in_length=8, out_length=8, stride=2,
+            CapsuleConv2d(in_channels=128, out_channels=128, kernel_size=3, in_length=8, out_length=16, stride=2,
                           padding=1),
-            CapsuleConv2d(in_channels=128, out_channels=256, kernel_size=3, in_length=8, out_length=16, stride=1,
-                          padding=1),
-            CapsuleConv2d(in_channels=256, out_channels=256, kernel_size=3, in_length=16, out_length=16, stride=1,
-                          padding=1),
-            CapsuleConv2d(in_channels=256, out_channels=256, kernel_size=3, in_length=16, out_length=16, stride=2,
-                          padding=1),
-            CapsuleConv2d(in_channels=256, out_channels=256, kernel_size=3, in_length=16, out_length=16, stride=1,
+            CapsuleConv2d(in_channels=128, out_channels=256, kernel_size=3, in_length=16, out_length=16, stride=1,
                           padding=1),
             CapsuleConv2d(in_channels=256, out_channels=256, kernel_size=3, in_length=16, out_length=16, stride=1,
                           padding=1),
             CapsuleConv2d(in_channels=256, out_channels=256, kernel_size=3, in_length=16, out_length=16, stride=2,
+                          padding=1),
+            CapsuleConv2d(in_channels=256, out_channels=256, kernel_size=3, in_length=16, out_length=32, stride=1,
+                          padding=1),
+            CapsuleConv2d(in_channels=256, out_channels=256, kernel_size=3, in_length=32, out_length=32, stride=1,
+                          padding=1),
+            CapsuleConv2d(in_channels=256, out_channels=256, kernel_size=3, in_length=32, out_length=32, stride=2,
                           padding=1)
         )
-        self.classifier = CapsuleLinear(in_capsules=2 * 2 * 256 // 16, out_capsules=10, in_length=16, out_length=16)
+        self.classifier = CapsuleLinear(in_capsules=2 * 2 * 256 // 32, out_capsules=10, in_length=32, out_length=32)
 
     def forward(self, x):
         out = self.features(x)
 
         out = out.view(*out.size()[:2], -1)
         out = out.transpose(-1, -2)
-        out = out.contiguous().view(out.size(0), -1, 16)
+        out = out.contiguous().view(out.size(0), -1, 32)
 
         out = self.classifier(out)
         classes = (out ** 2).sum(dim=-1) ** 0.5
