5870aa343e2030963394bc87d58728a9ba461298
leftthomas <leftthomas@qq.com>
2018-01-08 20:16:08 +0800

    change FashionMNISTCapsuleNet

--- model.py
+++ model.py
@@ -40,24 +40,20 @@ class FashionMNISTCapsuleNet(nn.Module):
     def __init__(self):
         super(FashionMNISTCapsuleNet, self).__init__()
         self.features = nn.Sequential(
-            CapsuleConv2d(in_channels=1, out_channels=64, kernel_size=3, in_length=1, out_length=8, stride=1,
+            CapsuleConv2d(in_channels=1, out_channels=32, kernel_size=3, in_length=1, out_length=8, stride=1,
                           padding=1),
-            CapsuleConv2d(in_channels=64, out_channels=64, kernel_size=3, in_length=8, out_length=8, stride=2,
+            CapsuleConv2d(in_channels=32, out_channels=32, kernel_size=3, in_length=8, out_length=8, stride=2,
                           padding=1),
-            CapsuleConv2d(in_channels=64, out_channels=128, kernel_size=3, in_length=8, out_length=8, stride=1,
+            CapsuleConv2d(in_channels=32, out_channels=64, kernel_size=3, in_length=8, out_length=16, stride=1,
                           padding=1),
-            CapsuleConv2d(in_channels=128, out_channels=128, kernel_size=3, in_length=16, out_length=8, stride=2,
+            CapsuleConv2d(in_channels=64, out_channels=64, kernel_size=3, in_length=16, out_length=16, stride=2,
                           padding=1),
-            CapsuleConv2d(in_channels=128, out_channels=256, kernel_size=3, in_length=16, out_length=16, stride=1,
+            CapsuleConv2d(in_channels=64, out_channels=128, kernel_size=3, in_length=16, out_length=16, stride=1,
                           padding=1),
-            CapsuleConv2d(in_channels=256, out_channels=256, kernel_size=3, in_length=16, out_length=16, stride=2,
-                          padding=1),
-            CapsuleConv2d(in_channels=256, out_channels=256, kernel_size=3, in_length=16, out_length=16, stride=1,
-                          padding=1),
-            CapsuleConv2d(in_channels=256, out_channels=256, kernel_size=3, in_length=16, out_length=16, stride=2,
+            CapsuleConv2d(in_channels=128, out_channels=128, kernel_size=3, in_length=16, out_length=16, stride=2,
                           padding=1),
         )
-        self.classifier = CapsuleLinear(in_capsules=2 * 2 * 256 // 16, out_capsules=10, in_length=16, out_length=16)
+        self.classifier = CapsuleLinear(in_capsules=4 * 4 * 128 // 16, out_capsules=10, in_length=16, out_length=16)
 
     def forward(self, x):
         out = self.features(x)
