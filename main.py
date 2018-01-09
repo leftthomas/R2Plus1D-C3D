ccd1987823b798164e05e349b643c84853c6ac72
leftthomas <leftthomas@qq.com>
2018-01-09 13:21:08 +0800

    add print debug

--- main.py
+++ main.py
@@ -25,7 +25,9 @@ def processor(sample):
         labels = labels.cuda()
 
     classes = model(data)
+    print(classes)
     loss = loss_criterion(classes, labels)
+    print(loss)
     return loss, classes
 
 
