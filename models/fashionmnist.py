90018ec141cbc48bcaaef2c0cb2360ed35c2efac
leftthomas <leftthomas@qq.com>
2018-01-26 15:14:13 +0800

    scale model by 2

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -7,35 +7,19 @@ from capsulelayer import CapsuleConv2d, CapsuleLinear
 class FashionMNISTCapsuleNet(nn.Module):
     def __init__(self):
         super(FashionMNISTCapsuleNet, self).__init__()
-        self.out_length = 8
+        self.out_length = 4
         self.features = nn.Sequential(
-            CapsuleConv2d(in_channels=1, out_channels=8, kernel_size=5, in_length=1, out_length=2, stride=1,
-                          padding=2),
+            CapsuleConv2d(in_channels=1, out_channels=8, kernel_size=7, in_length=1, out_length=2, stride=2,
+                          padding=0),
             nn.BatchNorm2d(num_features=8),
             nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=8, out_channels=8, kernel_size=3, in_length=2, out_length=2, stride=2,
-                          padding=1),
-            nn.BatchNorm2d(num_features=8),
-            nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=8, out_channels=16, kernel_size=3, in_length=2, out_length=4, stride=1,
-                          padding=1),
+            CapsuleConv2d(in_channels=8, out_channels=16, kernel_size=3, in_length=2, out_length=self.out_length,
+                          stride=2,
+                          padding=0),
             nn.BatchNorm2d(num_features=16),
-            nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=16, out_channels=16, kernel_size=3, in_length=4, out_length=4, stride=2,
-                          padding=1),
-            nn.BatchNorm2d(num_features=16),
-            nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=16, out_channels=32, kernel_size=3, in_length=4, out_length=8, stride=1,
-                          padding=1),
-            nn.BatchNorm2d(num_features=32),
-            nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=32, out_channels=32, kernel_size=3, in_length=8, out_length=self.out_length,
-                          stride=2, padding=1),
-            nn.BatchNorm2d(num_features=32),
             nn.ReLU(inplace=True)
-
         )
-        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=4 * 4 * 32 // self.out_length, out_capsules=10,
+        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=5 * 5 * 16 // self.out_length, out_capsules=10,
                                                       in_length=self.out_length, out_length=self.out_length),
                                         nn.ReLU(inplace=True))
 
