54b453f48a1847af28af8c13666744c5dc354bf5
leftthomas <leftthomas@qq.com>
2018-01-26 18:59:11 +0800

    change out_length to 4 of the first conv2d

--- models/mnist.py
+++ models/mnist.py
@@ -9,11 +9,11 @@ class MNISTCapsuleNet(nn.Module):
         super(MNISTCapsuleNet, self).__init__()
         self.out_length = 4
         self.features = nn.Sequential(
-            CapsuleConv2d(in_channels=1, out_channels=4, kernel_size=5, in_length=1, out_length=2, stride=2,
+            CapsuleConv2d(in_channels=1, out_channels=4, kernel_size=5, in_length=1, out_length=4, stride=2,
                           padding=0),
             nn.BatchNorm2d(num_features=4),
             nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=4, out_channels=8, kernel_size=5, in_length=2, out_length=self.out_length,
+            CapsuleConv2d(in_channels=4, out_channels=8, kernel_size=5, in_length=4, out_length=self.out_length,
                           stride=2,
                           padding=0),
             nn.BatchNorm2d(num_features=8),
