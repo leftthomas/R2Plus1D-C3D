84ba9f54acf0a933cc1511f6c01199856145b662
leftthomas <leftthomas@qq.com>
2018-02-01 19:57:45 +0800

    change model

--- models/mnist.py
+++ models/mnist.py
@@ -8,11 +8,11 @@ class MNISTCapsuleNet(nn.Module):
         super(MNISTCapsuleNet, self).__init__()
         self.out_length = 8
         self.features = nn.Sequential(
-            CapsuleConv2d(in_channels=1, out_channels=8, kernel_size=5, in_length=1, out_length=4, stride=1,
+            CapsuleConv2d(in_channels=1, out_channels=8, kernel_size=5, in_length=1, out_length=2, stride=1,
                           padding=2),
             nn.BatchNorm2d(num_features=8),
             nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=8, out_channels=16, kernel_size=3, in_length=4, out_length=4, stride=2,
+            CapsuleConv2d(in_channels=8, out_channels=16, kernel_size=3, in_length=2, out_length=4, stride=2,
                           padding=1),
             nn.BatchNorm2d(num_features=16),
             nn.ReLU(inplace=True),
