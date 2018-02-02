214fff3b9827dc5a1463ae15cfc9cd9e75350793
leftthomas <leftthomas@qq.com>
2018-02-03 00:34:39 +0800

    use capsule and sum version

--- models/cifar10.py
+++ models/cifar10.py
@@ -8,22 +8,18 @@ class CIFAR10CapsuleNet(nn.Module):
         super(CIFAR10CapsuleNet, self).__init__()
         self.out_length = 8
         self.features = nn.Sequential(
-            CapsuleConv2d(in_channels=3, out_channels=32, kernel_size=3, in_length=1, out_length=4, stride=1,
+            CapsuleConv2d(in_channels=3, out_channels=16, kernel_size=5, in_length=1, out_length=4, stride=1,
+                          padding=2),
+            nn.BatchNorm2d(num_features=16),
+            nn.ReLU(inplace=True),
+            CapsuleConv2d(in_channels=16, out_channels=32, kernel_size=3, in_length=4, out_length=8, stride=2,
                           padding=1),
             nn.BatchNorm2d(num_features=32),
             nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=32, out_channels=32, kernel_size=3, in_length=4, out_length=4, stride=2,
-                          padding=1),
-            nn.BatchNorm2d(num_features=32),
-            nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=32, out_channels=64, kernel_size=3, in_length=4, out_length=8, stride=1,
-                          padding=1),
+            CapsuleConv2d(in_channels=32, out_channels=64, kernel_size=3, in_length=8, out_length=self.out_length,
+                          stride=2, padding=1),
             nn.BatchNorm2d(num_features=64),
-            nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=64, out_channels=64, kernel_size=3, in_length=8, out_length=8, stride=2,
-                          padding=1),
-            nn.BatchNorm2d(num_features=64),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True)
         )
         self.classifier = CapsuleLinear(in_capsules=8 * 8 * 64 // self.out_length, out_capsules=10,
                                         in_length=self.out_length, out_length=self.out_length)
