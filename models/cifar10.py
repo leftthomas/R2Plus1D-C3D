d05a3ace6fe977dd840a06ac81029a362521e89e
leftthomas <leftthomas@qq.com>
2018-02-03 08:26:52 +0800

    add dynamic routing to all the layer, the last model got 60.62% acc at epoch 34

--- models/cifar10.py
+++ models/cifar10.py
@@ -9,20 +9,20 @@ class CIFAR10CapsuleNet(nn.Module):
         self.out_length = 8
         self.features = nn.Sequential(
             CapsuleConv2d(in_channels=3, out_channels=16, kernel_size=5, in_length=1, out_length=4, stride=1,
-                          padding=2),
+                          padding=2, with_routing=True),
             nn.BatchNorm2d(num_features=16),
             nn.ReLU(inplace=True),
             CapsuleConv2d(in_channels=16, out_channels=32, kernel_size=3, in_length=4, out_length=8, stride=2,
-                          padding=1),
+                          padding=1, with_routing=True),
             nn.BatchNorm2d(num_features=32),
             nn.ReLU(inplace=True),
             CapsuleConv2d(in_channels=32, out_channels=64, kernel_size=3, in_length=8, out_length=self.out_length,
-                          stride=2, padding=1),
+                          stride=2, padding=1, with_routing=True),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True)
         )
         self.classifier = CapsuleLinear(in_capsules=8 * 8 * 64 // self.out_length, out_capsules=10,
-                                        in_length=self.out_length, out_length=self.out_length)
+                                        in_length=self.out_length, out_length=self.out_length, with_routing=True)
 
     def forward(self, x):
         out = self.features(x)
