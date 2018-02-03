74e3d4f6f112288a8cd814f34d6b2fc7a45c1b2b
leftthomas <leftthomas@qq.com>
2018-02-04 05:29:50 +0800

    add with_routing=False to init model

--- models/cifar10.py
+++ models/cifar10.py
@@ -4,25 +4,26 @@ from capsulelayer import CapsuleLinear, CapsuleConv2d
 
 
 class CIFAR10CapsuleNet(nn.Module):
-    def __init__(self):
+    def __init__(self, with_routing=False):
         super(CIFAR10CapsuleNet, self).__init__()
         self.out_length = 8
         self.features = nn.Sequential(
             CapsuleConv2d(in_channels=3, out_channels=16, kernel_size=5, in_length=1, out_length=4, stride=1,
-                          padding=2, with_routing=True),
+                          padding=2, with_routing=with_routing),
             nn.BatchNorm2d(num_features=16),
             nn.ReLU(inplace=True),
             CapsuleConv2d(in_channels=16, out_channels=32, kernel_size=3, in_length=4, out_length=8, stride=2,
-                          padding=1, with_routing=True),
+                          padding=1, with_routing=with_routing),
             nn.BatchNorm2d(num_features=32),
             nn.ReLU(inplace=True),
             CapsuleConv2d(in_channels=32, out_channels=64, kernel_size=3, in_length=8, out_length=self.out_length,
-                          stride=2, padding=1, with_routing=True),
+                          stride=2, padding=1, with_routing=with_routing),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True)
         )
         self.classifier = CapsuleLinear(in_capsules=8 * 8 * 64 // self.out_length, out_capsules=10,
-                                        in_length=self.out_length, out_length=self.out_length, with_routing=True)
+                                        in_length=self.out_length, out_length=self.out_length,
+                                        with_routing=with_routing)
 
     def forward(self, x):
         out = self.features(x)
--- models/cifar100.py
+++ models/cifar100.py
@@ -1,31 +1,31 @@
-import torch.nn.functional as F
 from torch import nn
 
 from capsulelayer import CapsuleConv2d, CapsuleLinear
 
 
 class CIFAR100CapsuleNet(nn.Module):
-    def __init__(self):
+    def __init__(self, with_routing=False):
         super(CIFAR100CapsuleNet, self).__init__()
         self.out_length = 16
         self.features = nn.Sequential(
             CapsuleConv2d(in_channels=3, out_channels=16, kernel_size=5, in_length=1, out_length=4, stride=1,
-                          padding=2),
+                          padding=2, with_routing=with_routing),
             CapsuleConv2d(in_channels=16, out_channels=16, kernel_size=5, in_length=4, out_length=4, stride=2,
-                          padding=2),
+                          padding=2, with_routing=with_routing),
             CapsuleConv2d(in_channels=16, out_channels=32, kernel_size=3, in_length=4, out_length=8, stride=1,
-                          padding=1),
+                          padding=1, with_routing=with_routing),
             CapsuleConv2d(in_channels=32, out_channels=32, kernel_size=3, in_length=8, out_length=8, stride=2,
-                          padding=1),
+                          padding=1, with_routing=with_routing),
             CapsuleConv2d(in_channels=32, out_channels=64, kernel_size=3, in_length=8, out_length=16, stride=1,
-                          padding=1),
+                          padding=1, with_routing=with_routing),
             CapsuleConv2d(in_channels=64, out_channels=64, kernel_size=3, in_length=16, out_length=16, stride=1,
-                          padding=1),
+                          padding=1, with_routing=with_routing),
             CapsuleConv2d(in_channels=64, out_channels=128, kernel_size=3, in_length=16, out_length=self.out_length,
-                          stride=2, padding=1)
+                          stride=2, padding=1, with_routing=with_routing)
         )
         self.classifier = CapsuleLinear(in_capsules=4 * 4 * 128 // self.out_length, out_capsules=100,
-                                        in_length=self.out_length, out_length=16)
+                                        in_length=self.out_length, out_length=self.out_length,
+                                        with_routing=with_routing)
 
     def forward(self, x):
         out = self.features(x)
@@ -35,6 +35,5 @@ class CIFAR100CapsuleNet(nn.Module):
         out = out.contiguous().view(out.size(0), -1, self.out_length)
 
         out = self.classifier(out)
-        classes = out.norm(p=2, dim=-1)
-        classes = F.softmax(classes, dim=-1)
+        classes = out.sum(dim=-1)
         return classes
