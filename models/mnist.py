4c7a59a73f6b2efee087fbac74904850b1eef64d
leftthomas <leftthomas@qq.com>
2018-02-04 05:10:02 +0800

    change model, the last model got 98.82% acc

--- models/mnist.py
+++ models/mnist.py
@@ -6,24 +6,24 @@ from capsulelayer import CapsuleConv2d, CapsuleLinear
 class MNISTCapsuleNet(nn.Module):
     def __init__(self):
         super(MNISTCapsuleNet, self).__init__()
-        self.out_length = 4
+        self.out_length = 8
         self.features = nn.Sequential(
-            CapsuleConv2d(in_channels=1, out_channels=8, kernel_size=5, in_length=1, out_length=2, stride=1,
-                          padding=2, with_routing=True),
-            nn.BatchNorm2d(num_features=8),
-            nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=8, out_channels=16, kernel_size=3, in_length=2, out_length=4, stride=2,
-                          padding=1, with_routing=True),
+            CapsuleConv2d(in_channels=1, out_channels=16, kernel_size=5, in_length=1, out_length=4, stride=1,
+                          padding=2),
             nn.BatchNorm2d(num_features=16),
             nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=16, out_channels=16, kernel_size=3, in_length=4, out_length=self.out_length,
+            CapsuleConv2d(in_channels=16, out_channels=32, kernel_size=3, in_length=4, out_length=4, stride=2,
+                          padding=1),
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(inplace=True),
+            CapsuleConv2d(in_channels=32, out_channels=32, kernel_size=3, in_length=4, out_length=self.out_length,
                           stride=2,
-                          padding=1, with_routing=True),
-            nn.BatchNorm2d(num_features=16),
+                          padding=1),
+            nn.BatchNorm2d(num_features=32),
             nn.ReLU(inplace=True)
         )
-        self.classifier = CapsuleLinear(in_capsules=7 * 7 * 16 // self.out_length, out_capsules=10,
-                                        in_length=self.out_length, out_length=self.out_length, with_routing=True)
+        self.classifier = CapsuleLinear(in_capsules=7 * 7 * 32 // self.out_length, out_capsules=10,
+                                        in_length=self.out_length, out_length=self.out_length)
 
     def forward(self, x):
         out = self.features(x)
