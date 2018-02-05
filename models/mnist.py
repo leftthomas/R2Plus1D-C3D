2f43765fd59794b0d8d167225b98163b1b7d24ea
leftthomas <leftthomas@qq.com>
2018-02-05 19:22:33 +0800

    using ReLU to replace LeakyReLU, the last model got 99.18% acc at epoch 95

--- models/mnist.py
+++ models/mnist.py
@@ -11,20 +11,20 @@ class MNISTCapsuleNet(nn.Module):
             CapsuleConv2d(in_channels=1, out_channels=16, kernel_size=5, in_length=1, out_length=4, stride=1,
                           padding=2, with_routing=with_routing),
             nn.BatchNorm2d(num_features=16),
-            nn.LeakyReLU(inplace=True),
+            nn.ReLU(inplace=True),
             CapsuleConv2d(in_channels=16, out_channels=32, kernel_size=3, in_length=4, out_length=4, stride=2,
                           padding=1, with_routing=with_routing),
             nn.BatchNorm2d(num_features=32),
-            nn.LeakyReLU(inplace=True),
+            nn.ReLU(inplace=True),
             CapsuleConv2d(in_channels=32, out_channels=32, kernel_size=3, in_length=4, out_length=self.out_length,
                           stride=2, padding=1, with_routing=with_routing),
             nn.BatchNorm2d(num_features=32),
-            nn.LeakyReLU(inplace=True)
+            nn.ReLU(inplace=True)
         )
         self.classifier = nn.Sequential(CapsuleLinear(in_capsules=7 * 7 * 32 // self.out_length, out_capsules=10,
                                                       in_length=self.out_length, out_length=self.out_length,
                                                       with_routing=with_routing),
-                                        nn.LeakyReLU(inplace=True))
+                                        nn.ReLU(inplace=True))
 
     def forward(self, x):
         out = self.features(x)
