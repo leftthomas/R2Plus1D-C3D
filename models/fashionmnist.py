bde453b544477166b01ba827ffe9b7729f542589
leftthomas <leftthomas@qq.com>
2018-02-06 16:55:20 +0800

    remove ReLU in the last layer, the last model got 91.69% acc at epoch 92

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -21,10 +21,9 @@ class FashionMNISTCapsuleNet(nn.Module):
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True)
         )
-        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=7 * 7 * 64 // self.out_length, out_capsules=10,
-                                                      in_length=self.out_length, out_length=self.out_length,
-                                                      with_routing=with_routing),
-                                        nn.ReLU(inplace=True))
+        self.classifier = CapsuleLinear(in_capsules=7 * 7 * 64 // self.out_length, out_capsules=10,
+                                        in_length=self.out_length, out_length=self.out_length,
+                                        with_routing=with_routing)
 
     def forward(self, x):
         out = self.features(x)
