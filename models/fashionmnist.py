1f47e4f3d4fe084022bc304ca28de44f229e68af
leftthomas <leftthomas@qq.com>
2018-02-06 17:18:29 +0800

    fix bug

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -6,7 +6,7 @@ from capsulelayer import CapsuleConv2d, CapsuleLinear
 class FashionMNISTCapsuleNet(nn.Module):
     def __init__(self, with_routing=False):
         super(FashionMNISTCapsuleNet, self).__init__()
-        self.out_length = 8
+        self.out_length = 16
         self.features = nn.Sequential(
             CapsuleConv2d(in_channels=1, out_channels=16, kernel_size=3, in_length=1, out_length=4, stride=1,
                           padding=1, with_routing=with_routing),
