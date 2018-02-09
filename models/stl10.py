b484126836f2522e423aa4c9582daeb77c7c3417
leftthomas <leftthomas@qq.com>
2018-02-09 10:57:57 +0800

    change model

--- models/stl10.py
+++ models/stl10.py
@@ -6,9 +6,9 @@ from capsulelayer import CapsuleConv2d, CapsuleLinear
 class STL10CapsuleNet(nn.Module):
     def __init__(self, with_conv_routing=False, with_linear_routing=False):
         super(STL10CapsuleNet, self).__init__()
-        self.out_length = 16
+        self.out_length = 32
         self.features = nn.Sequential(
-            CapsuleConv2d(in_channels=3, out_channels=16, kernel_size=3, in_length=1, out_length=4, stride=2,
+            CapsuleConv2d(in_channels=3, out_channels=16, kernel_size=3, in_length=1, out_length=4, stride=1,
                           padding=1, with_routing=with_conv_routing),
             nn.BatchNorm2d(num_features=16),
             nn.ReLU(inplace=True),
@@ -18,34 +18,27 @@ class STL10CapsuleNet(nn.Module):
             nn.BatchNorm2d(num_features=32),
             nn.ReLU(inplace=True),
 
-            CapsuleConv2d(in_channels=32, out_channels=64, kernel_size=3, in_length=8, out_length=8, stride=1,
+            CapsuleConv2d(in_channels=32, out_channels=64, kernel_size=3, in_length=8, out_length=16, stride=1,
                           padding=1, with_routing=with_conv_routing),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=64, out_channels=64, kernel_size=3, in_length=8, out_length=8, stride=2,
+            CapsuleConv2d(in_channels=64, out_channels=64, kernel_size=3, in_length=16, out_length=16, stride=2,
                           padding=1, with_routing=with_conv_routing),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
 
-            CapsuleConv2d(in_channels=64, out_channels=128, kernel_size=3, in_length=8, out_length=16, stride=1,
+            CapsuleConv2d(in_channels=64, out_channels=128, kernel_size=3, in_length=16, out_length=32, stride=1,
                           padding=1, with_routing=with_conv_routing),
             nn.BatchNorm2d(num_features=128),
             nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=128, out_channels=128, kernel_size=3, in_length=16, out_length=16, stride=2,
+            CapsuleConv2d(in_channels=128, out_channels=128, kernel_size=3, in_length=32, out_length=self.out_length,
+                          stride=2,
                           padding=1, with_routing=with_conv_routing),
             nn.BatchNorm2d(num_features=128),
             nn.ReLU(inplace=True),
 
-            CapsuleConv2d(in_channels=128, out_channels=128, kernel_size=3, in_length=16, out_length=16, stride=1,
-                          padding=1, with_routing=with_conv_routing),
-            nn.BatchNorm2d(num_features=128),
-            nn.ReLU(inplace=True),
-            CapsuleConv2d(in_channels=128, out_channels=128, kernel_size=3, in_length=16, out_length=self.out_length,
-                          stride=2, padding=1, with_routing=with_conv_routing),
-            nn.BatchNorm2d(num_features=128),
-            nn.ReLU(inplace=True),
         )
-        self.classifier = CapsuleLinear(in_capsules=3 * 3 * 128 // self.out_length, out_capsules=10,
+        self.classifier = CapsuleLinear(in_capsules=12 * 12 * 128 // self.out_length, out_capsules=10,
                                         in_length=self.out_length, out_length=self.out_length,
                                         with_routing=with_linear_routing)
 
