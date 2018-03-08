6365900c0dcaf646b19cafeeb6dc3ff7d7e8ff63
leftthomas <leftthomas@qq.com>
2018-03-08 10:53:43 +0800

    change kernel size

--- models/mnist.py
+++ models/mnist.py
@@ -5,23 +5,27 @@ from torch import nn
 class MNISTCapsuleNet(nn.Module):
     def __init__(self, routing_type='sum'):
         super(MNISTCapsuleNet, self).__init__()
-        self.features_out_length = 4
+        self.features_out_length = 8
         self.features = nn.Sequential(
             CapsuleConv2d(in_channels=1, out_channels=64, kernel_size=7, in_length=1, out_length=4, stride=1,
                           padding=0),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
-
-            CapsuleConv2d(in_channels=64, out_channels=64, kernel_size=5, in_length=4,
-                          out_length=self.features_out_length, stride=2, padding=0),
+            CapsuleConv2d(in_channels=64, out_channels=64, kernel_size=5, in_length=4, out_length=4, stride=2,
+                          padding=0),
             nn.BatchNorm2d(num_features=64),
+            nn.ReLU(inplace=True),
+
+            CapsuleConv2d(in_channels=64, out_channels=128, kernel_size=3, in_length=4,
+                          out_length=self.features_out_length, stride=2, padding=0),
+            nn.BatchNorm2d(num_features=128),
             nn.ReLU(inplace=True)
         )
         self.classifier = nn.Sequential(
-            CapsuleLinear(in_capsules=9 * 9 * 64 // self.features_out_length, out_capsules=128,
+            CapsuleLinear(in_capsules=4 * 4 * 128 // self.features_out_length, out_capsules=32,
                           in_length=self.features_out_length, out_length=8, routing_type=routing_type,
                           share_weight=True),
-            CapsuleLinear(in_capsules=128, out_capsules=10, in_length=8, out_length=16, routing_type=routing_type))
+            CapsuleLinear(in_capsules=32, out_capsules=10, in_length=8, out_length=16, routing_type=routing_type))
 
     def forward(self, x):
         out = self.features(x)
