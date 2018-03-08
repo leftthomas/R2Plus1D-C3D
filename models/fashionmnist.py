906b5129f9ff3797fb9e920f6cae9840cf8e2604
leftthomas <leftthomas@qq.com>
2018-03-08 21:39:15 +0800

    change model

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -6,23 +6,21 @@ class FashionMNISTCapsuleNet(nn.Module):
     def __init__(self, routing_type='sum'):
         super(FashionMNISTCapsuleNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=1, padding=0),
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=7, stride=1, padding=0),
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=2, padding=0),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, stride=2, padding=0),
-            nn.BatchNorm2d(num_features=128),
-            nn.ReLU(inplace=True),
 
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=0),
-            nn.BatchNorm2d(num_features=256),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=0),
+            nn.BatchNorm2d(num_features=128),
             nn.ReLU(inplace=True)
         )
         self.classifier = nn.Sequential(
-            CapsuleLinear(in_capsules=1024, out_capsules=256, in_length=4, out_length=8, routing_type=routing_type,
+            CapsuleLinear(in_capsules=512, out_capsules=128, in_length=4, out_length=8, routing_type=routing_type,
                           share_weight=True),
-            CapsuleLinear(in_capsules=256, out_capsules=64, in_length=8, out_length=12, routing_type=routing_type,
-                          share_weight=True),
-            CapsuleLinear(in_capsules=64, out_capsules=10, in_length=12, out_length=16, routing_type=routing_type,
+            CapsuleLinear(in_capsules=128, out_capsules=10, in_length=8, out_length=16, routing_type=routing_type,
                           share_weight=True))
 
     def forward(self, x):
