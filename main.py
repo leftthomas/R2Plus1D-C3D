b6ada531272d7af86b04229464d60ac6c2a962c2
leftthomas <leftthomas@qq.com>
2018-03-08 16:18:57 +0800

    fix bugs

--- main.py
+++ main.py
@@ -97,7 +97,10 @@ def on_end_epoch(state):
 
     # features visualization
     original_image, _ = next(iter(utils.get_iterator(False, DATA_TYPE, 25, USE_DATA_AUGMENTATION)))
-    features = utils.show_features(model, TARGET_LAYER, Variable(original_image))
+    data = Variable(original_image)
+    if torch.cuda.is_available():
+        data = data.cuda()
+    features = utils.show_features(model, TARGET_LAYER, data)
     original_image_logger.log(make_grid(original_image, nrow=5, normalize=True).numpy())
     features_logger.log(make_grid(features, nrow=5, normalize=True).numpy())
 
