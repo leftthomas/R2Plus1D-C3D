edcdd0a807ae0611275c3c333c3bf9cc4647bdd7
leftthomas <leftthomas@qq.com>
2018-03-09 18:49:55 +0800

    use two classfier and share weight

--- models/mnist.py
+++ models/mnist.py
@@ -17,8 +17,10 @@ class MNISTCapsuleNet(nn.Module):
             nn.BatchNorm2d(num_features=128),
             nn.ReLU(inplace=True)
         )
-        self.classifier = CapsuleLinear(in_capsules=256, out_capsules=10, in_length=8, out_length=16,
-                                        routing_type=routing_type, share_weight=False)
+        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=256, out_capsules=64, in_length=8, out_length=12,
+                                                      routing_type=routing_type, share_weight=True),
+                                        CapsuleLinear(in_capsules=64, out_capsules=10, in_length=12, out_length=16,
+                                                      routing_type=routing_type, share_weight=False))
 
     def forward(self, x):
         out = self.features(x)
