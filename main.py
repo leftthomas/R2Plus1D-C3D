6be91832dd2e9d140cb6ff40ea25254170451273
leftthomas <leftthomas@qq.com>
2018-03-09 18:57:31 +0800

    make features visualization don't use gpu mem

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def on_end_epoch(state):
 
     # features visualization
     original_image, _ = next(iter(utils.get_iterator(False, DATA_TYPE, 25, USE_DA)))
-    data = Variable(original_image, requires_grad=False)
+    data = Variable(original_image, volatile=True)
     if torch.cuda.is_available():
         data = data.cuda()
     features = utils.show_features(model, TARGET_LAYER, data)
