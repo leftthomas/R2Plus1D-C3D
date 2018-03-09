ba8bc01040faf96560dc1fac0d0ed74aeb35a98f
leftthomas <leftthomas@qq.com>
2018-03-09 16:28:52 +0800

    change model of mnist

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -18,9 +18,9 @@ class FashionMNISTCapsuleNet(nn.Module):
             nn.ReLU(inplace=True)
         )
         self.classifier = nn.Sequential(
-            CapsuleLinear(in_capsules=256, out_capsules=64, in_length=8, out_length=8, routing_type=routing_type,
+            CapsuleLinear(in_capsules=256, out_capsules=128, in_length=8, out_length=8, routing_type=routing_type,
                           share_weight=False),
-            CapsuleLinear(in_capsules=64, out_capsules=10, in_length=8, out_length=16, routing_type=routing_type,
+            CapsuleLinear(in_capsules=128, out_capsules=10, in_length=8, out_length=16, routing_type=routing_type,
                           share_weight=False))
 
     def forward(self, x):
