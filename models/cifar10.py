b2a74afcdb4cb19af03a959233a7d6b3da40dd38
leftthomas <leftthomas@qq.com>
2018-03-09 19:09:27 +0800

    make features visualization don't use gpu mem

--- models/cifar10.py
+++ models/cifar10.py
@@ -41,9 +41,9 @@ class CIFAR10CapsuleNet(nn.Module):
             nn.ReLU(inplace=True)
         )
         self.classifier = nn.Sequential(CapsuleLinear(in_capsules=256, out_capsules=64, in_length=8, out_length=12,
-                                                      routing_type=routing_type, share_weight=True),
+                                                      routing_type=routing_type, share_weight=False),
                                         CapsuleLinear(in_capsules=64, out_capsules=10, in_length=12, out_length=16,
-                                                      routing_type=routing_type, share_weight=False))
+                                                      routing_type=routing_type, share_weight=True))
 
     def forward(self, x):
         out = self.features(x)
