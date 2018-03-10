79d65314f0119d5df8609f626ddc24869b1d0ed2
leftthomas <leftthomas@qq.com>
2018-03-10 20:09:32 +0800

    change model

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -9,59 +9,56 @@ class FashionMNISTCapsuleNet(nn.Module):
             nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, stride=1, padding=2, bias=False),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True)
         )
         self.bk1 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=64),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=64)
-        )
-        self.bk2 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(num_features=128),
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(num_features=128),
         )
-        self.bk3 = nn.Sequential(
+        self.bk2 = nn.Sequential(
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(num_features=256),
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(num_features=256),
         )
-        self.bk4 = nn.Sequential(
+        self.bk3 = nn.Sequential(
             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(num_features=512),
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(num_features=512),
         )
-        self.down2 = nn.Sequential(
+        self.down1 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=1, stride=2, bias=False),
             nn.BatchNorm2d(128),
         )
-        self.down3 = nn.Sequential(
+        self.down2 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=1, stride=2, bias=False),
             nn.BatchNorm2d(256),
         )
-        self.down4 = nn.Sequential(
+        self.down3 = nn.Sequential(
             nn.Conv2d(256, 512, kernel_size=1, stride=2, bias=False),
             nn.BatchNorm2d(512),
         )
         self.relu = nn.ReLU(inplace=True)
-        self.classifier = CapsuleLinear(in_capsules=256, out_capsules=10, in_length=8, out_length=16,
-                                        routing_type=routing_type, share_weight=False, num_iterations=num_iterations)
+        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=1024, out_capsules=256, in_length=8, out_length=16,
+                                                      routing_type=routing_type, share_weight=True,
+                                                      num_iterations=num_iterations),
+                                        CapsuleLinear(in_capsules=256, out_capsules=10, in_length=16, out_length=32,
+                                                      routing_type=routing_type, share_weight=False,
+                                                      num_iterations=num_iterations))
 
     def forward(self, x):
         out = self.features(x)
         res = out
         out = self.bk1(out)
-        out += res
+        out += self.down1(res)
         out = self.relu(out)
         res = out
         out = self.bk2(out)
@@ -70,10 +67,6 @@ class FashionMNISTCapsuleNet(nn.Module):
         res = out
         out = self.bk3(out)
         out += self.down3(res)
-        out = self.relu(out)
-        res = out
-        out = self.bk4(out)
-        out += self.down4(res)
         out = self.relu(out)
 
         out = out.view(*out.size()[:2], -1)
