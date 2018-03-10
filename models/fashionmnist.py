978d8af838511812503096c84ede8a72a1f087f1
leftthomas <leftthomas@qq.com>
2018-03-10 19:29:03 +0800

    fix bug

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -2,43 +2,6 @@ from capsule_layer import CapsuleLinear
 from torch import nn
 
 
-class BasicBlock(nn.Module):
-
-    def __init__(self, in_channels, out_channels, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.stride = stride
-        self.downsample = None
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.stride == 2:
-            self.downsample = nn.Sequential(
-                nn.Conv2d(self.in_channels, self.out_channels, kernel_size=1, stride=self.stride, bias=False),
-                nn.BatchNorm2d(self.out_channels),
-            )
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
 class FashionMNISTCapsuleNet(nn.Module):
     def __init__(self, routing_type='sum', num_iterations=3):
         super(FashionMNISTCapsuleNet, self).__init__()
@@ -50,19 +13,68 @@ class FashionMNISTCapsuleNet(nn.Module):
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True)
         )
-        self.bk1 = BasicBlock(in_channels=64, out_channels=64)
-        self.bk2 = BasicBlock(in_channels=64, out_channels=128, stride=2)
-        self.bk3 = BasicBlock(in_channels=128, out_channels=256, stride=2)
-        self.bk4 = BasicBlock(in_channels=256, out_channels=512, stride=2)
+        self.bk1 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=64)
+        )
+        self.bk2 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=128),
+        )
+        self.bk3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=256),
+        )
+        self.bk4 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=512),
+        )
+        self.down2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(128),
+        )
+        self.down3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(256),
+        )
+        self.down4 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(512),
+        )
+        self.relu = nn.ReLU(inplace=True)
         self.classifier = CapsuleLinear(in_capsules=256, out_capsules=10, in_length=8, out_length=16,
                                         routing_type=routing_type, share_weight=False, num_iterations=num_iterations)
 
     def forward(self, x):
         out = self.features(x)
+        res = out
         out = self.bk1(out)
+        out += res
+        out = self.relu(out)
+        res = out
         out = self.bk2(out)
+        out += self.down2(res)
+        out = self.relu(out)
+        res = out
         out = self.bk3(out)
+        out += self.down3(res)
+        out = self.relu(out)
+        res = out
         out = self.bk4(out)
+        out += self.down4(res)
+        out = self.relu(out)
 
         out = out.view(*out.size()[:2], -1)
         out = out.transpose(-1, -2)
