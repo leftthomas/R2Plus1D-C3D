49a3acc1834ed4ec418d3bd27ec4939340a2e0da
leftthomas <leftthomas@qq.com>
2018-03-10 22:40:32 +0800

    change model

--- models/stl10.py
+++ models/stl10.py
@@ -6,58 +6,109 @@ class STL10CapsuleNet(nn.Module):
     def __init__(self, routing_type='sum', num_iterations=3):
         super(STL10CapsuleNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=1, padding=3, bias=False),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2, bias=False),
             nn.BatchNorm2d(num_features=64),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=128),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(num_features=128),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(num_features=256),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(num_features=512),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(num_features=512),
             nn.ReLU(inplace=True)
         )
-        self.classifier = nn.Sequential(
-            CapsuleLinear(in_capsules=576, out_capsules=144, in_length=8, out_length=12, routing_type=routing_type,
-                          share_weight=False, num_iterations=num_iterations),
-            CapsuleLinear(in_capsules=144, out_capsules=10, in_length=12, out_length=16, routing_type=routing_type,
-                          share_weight=False, num_iterations=num_iterations))
+        self.bk1 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=128),
+        )
+        self.bk2 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=128),
+        )
+        self.bk3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=256),
+        )
+        self.bk4 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=256),
+        )
+        self.bk5 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=512),
+        )
+        self.bk6 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(num_features=512),
+        )
+        self.down1 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(128),
+        )
+        self.down2 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(128),
+        )
+        self.down3 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(256),
+        )
+        self.down4 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(256),
+        )
+        self.down5 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(512),
+        )
+        self.down6 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(512),
+        )
+        self.relu = nn.ReLU(inplace=True)
+        self.classifier = CapsuleLinear(in_capsules=256, out_capsules=10, in_length=8, out_length=16,
+                                        routing_type=routing_type, share_weight=False, num_iterations=num_iterations)
 
     def forward(self, x):
         out = self.features(x)
+        res = out
+        out = self.bk1(out)
+        out += self.down1(res)
+        out = self.relu(out)
+        res = out
+        out = self.bk2(out)
+        out += self.down2(res)
+        out = self.relu(out)
+        res = out
+        out = self.bk3(out)
+        out += self.down3(res)
+        out = self.relu(out)
+        res = out
+        out = self.bk4(out)
+        out += self.down4(res)
+        out = self.relu(out)
+        res = out
+        out = self.bk5(out)
+        out += self.down5(res)
+        out = self.relu(out)
+        res = out
+        out = self.bk6(out)
+        out += self.down6(res)
+        out = self.relu(out)
 
         out = out.view(*out.size()[:2], -1)
         out = out.transpose(-1, -2)
