66160271e8ffe627bb229c21cb5379fad2eae2de
leftthomas <leftthomas@qq.com>
2018-03-18 19:46:22 +0800

    change classfired

--- models/cifar10.py
+++ models/cifar10.py
@@ -6,10 +6,17 @@ class CIFAR10CapsuleNet(nn.Module):
     def __init__(self, routing_type='sum', num_iterations=3):
         super(CIFAR10CapsuleNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3),
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(num_features=32),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
 
@@ -18,21 +25,14 @@ class CIFAR10CapsuleNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(num_features=128),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(num_features=256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=256),
             nn.ReLU(inplace=True)
 
         )
-        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=512, out_capsules=128, in_length=8, out_length=12,
-                                                      routing_type=routing_type, share_weight=True,
+        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=256, out_capsules=64, in_length=8, out_length=12,
+                                                      routing_type='dynamic', share_weight=True,
                                                       num_iterations=num_iterations),
-                                        CapsuleLinear(in_capsules=128, out_capsules=10, in_length=12, out_length=16,
-                                                      routing_type=routing_type, share_weight=False,
+                                        CapsuleLinear(in_capsules=64, out_capsules=10, in_length=12, out_length=16,
+                                                      routing_type='contract', share_weight=False,
                                                       num_iterations=num_iterations))
 
     def forward(self, x):
