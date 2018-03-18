bc5b510baeb00b722bfb34e33d0fee4fe8393f21
leftthomas <leftthomas@qq.com>
2018-03-18 13:07:17 +0800

    add bias

--- models/cifar10.py
+++ models/cifar10.py
@@ -6,30 +6,30 @@ class CIFAR10CapsuleNet(nn.Module):
     def __init__(self, routing_type='sum', num_iterations=3):
         super(CIFAR10CapsuleNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2, bias=False),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(num_features=128),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(num_features=128),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(num_features=256),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(num_features=256),
             nn.ReLU(inplace=True)
 
         )
         self.classifier = CapsuleLinear(in_capsules=512, out_capsules=10, in_length=8, out_length=16,
-                                        routing_type=routing_type, share_weight=True, num_iterations=num_iterations)
+                                        routing_type=routing_type, share_weight=False, num_iterations=num_iterations)
 
     def forward(self, x):
         out = self.features(x)
