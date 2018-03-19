640cd4876d28b09717ead5906b7808a9da185f5a
leftthomas <leftthomas@qq.com>
2018-03-19 22:28:51 +0800

    change model

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -16,6 +16,9 @@ class FashionMNISTCapsuleNet(nn.Module):
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=64),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
@@ -34,13 +37,10 @@ class FashionMNISTCapsuleNet(nn.Module):
             nn.BatchNorm2d(num_features=128),
             nn.ReLU(inplace=True)
         )
-        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=128, out_capsules=96, in_length=4, out_length=6,
+        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=128, out_capsules=64, in_length=4, out_length=6,
                                                       routing_type='dynamic', share_weight=True,
                                                       num_iterations=num_iterations),
-                                        CapsuleLinear(in_capsules=96, out_capsules=64, in_length=6, out_length=8,
-                                                      routing_type='contract', share_weight=False,
-                                                      num_iterations=num_iterations),
-                                        CapsuleLinear(in_capsules=64, out_capsules=10, in_length=8, out_length=10,
+                                        CapsuleLinear(in_capsules=64, out_capsules=10, in_length=6, out_length=8,
                                                       routing_type='contract', share_weight=False,
                                                       num_iterations=num_iterations))
 
