b719e3a74ab6c6518692ade7eeb9cfa96a26d888
leftthomas <leftthomas@qq.com>
2018-03-20 15:23:59 +0800

    change model

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -6,41 +6,50 @@ class FashionMNISTCapsuleNet(nn.Module):
     def __init__(self, num_iterations=3):
         super(FashionMNISTCapsuleNet, self).__init__()
         self.block1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=32),
+            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=64),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=32)
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=64)
         )
         self.block2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=64),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(num_features=64),
-        )
-        self.block3 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(num_features=128),
             nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(num_features=128)
         )
-        self.block4 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(num_features=128),
+        self.block3 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=256),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(num_features=128)
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(num_features=256)
         )
-        self.down_block1 = nn.Sequential(nn.Conv2d(in_channels=1, out_channels=32, kernel_size=1, stride=1),
-                                         nn.BatchNorm2d(num_features=32))
-        self.down_block2 = nn.Sequential(nn.Conv2d(in_channels=32, out_channels=64, kernel_size=1, stride=2),
+        self.block4 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(num_features=512)
+        )
+        self.block5 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(num_features=512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(num_features=512)
+        )
+        self.down_block1 = nn.Sequential(nn.Conv2d(in_channels=1, out_channels=64, kernel_size=1, stride=1),
                                          nn.BatchNorm2d(num_features=64))
-        self.down_block3 = nn.Sequential(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=1, stride=2),
+        self.down_block2 = nn.Sequential(nn.Conv2d(in_channels=64, out_channels=128, kernel_size=1, stride=2),
                                          nn.BatchNorm2d(num_features=128))
-        self.down_block4 = nn.Sequential(nn.Conv2d(in_channels=128, out_channels=128, kernel_size=1, stride=2),
-                                         nn.BatchNorm2d(num_features=128))
+        self.down_block3 = nn.Sequential(nn.Conv2d(in_channels=128, out_channels=256, kernel_size=1, stride=2),
+                                         nn.BatchNorm2d(num_features=256))
+        self.down_block4 = nn.Sequential(nn.Conv2d(in_channels=256, out_channels=512, kernel_size=1, stride=2),
+                                         nn.BatchNorm2d(num_features=512))
+        self.down_block5 = nn.Sequential(nn.Conv2d(in_channels=512, out_channels=512, kernel_size=1, stride=2),
+                                         nn.BatchNorm2d(num_features=512))
         self.relu = nn.ReLU(inplace=True)
         self.classifier = nn.Sequential(CapsuleLinear(in_capsules=512, out_capsules=128, in_length=4, out_length=8,
                                                       routing_type='dynamic', share_weight=False,
@@ -68,6 +77,11 @@ class FashionMNISTCapsuleNet(nn.Module):
         features = out
         out = self.block4(features)
         out += self.down_block4(features)
+        out = self.relu(out)
+
+        features = out
+        out = self.block5(features)
+        out += self.down_block5(features)
         out = self.relu(out)
 
         out = out.view(*out.size()[:2], -1)
