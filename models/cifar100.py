db224e9377906e8258d4b945630bb1b1b6b4f0eb
leftthomas <leftthomas@qq.com>
2018-03-22 18:25:20 +0800

    change model

--- models/cifar100.py
+++ models/cifar100.py
@@ -15,7 +15,7 @@ class CIFAR100CapsuleNet(nn.Module):
             layers.append(module)
         self.features = nn.Sequential(*layers)
         self.pool = nn.AvgPool2d(kernel_size=4)
-        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=128, out_capsules=100, in_length=4, out_length=8,
+        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=512, out_capsules=100, in_length=4, out_length=8,
                                                       routing_type='contract', share_weight=False,
                                                       num_iterations=num_iterations))
 
