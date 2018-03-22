b11c35d533d3081d9dee00eedb08803f00e2cafe
leftthomas <leftthomas@qq.com>
2018-03-22 16:27:14 +0800

    change model

--- models/stl10.py
+++ models/stl10.py
@@ -1,88 +1,31 @@
 from capsule_layer import CapsuleLinear
 from torch import nn
+from torchvision.models.resnet import resnet18
 
 
 class STL10CapsuleNet(nn.Module):
     def __init__(self, num_iterations=3):
         super(STL10CapsuleNet, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=1, padding=3, bias=False),
-            nn.BatchNorm2d(num_features=64),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=2, padding=2, bias=False),
-            nn.BatchNorm2d(num_features=64),
-            nn.ReLU(inplace=True)
-        )
-        self.bk1 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=128),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=128),
-        )
-        self.bk2 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=256),
-        )
-        self.bk3 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=512),
-        )
-        self.bk4 = nn.Sequential(
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=512),
-        )
-        self.down1 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=1, stride=2, bias=False),
-            nn.BatchNorm2d(128),
-        )
-        self.down2 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=1, stride=2, bias=False),
-            nn.BatchNorm2d(256),
-        )
-        self.down3 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=1, stride=2, bias=False),
-            nn.BatchNorm2d(512),
-        )
-        self.down4 = nn.Sequential(
-            nn.Conv2d(512, 512, kernel_size=1, stride=2, bias=False),
-            nn.BatchNorm2d(512),
-        )
-        self.relu = nn.ReLU(inplace=True)
-        self.classifier = CapsuleLinear(in_capsules=576, out_capsules=10, in_length=8, out_length=16,
-                                        routing_type='dynamic', share_weight=False, num_iterations=num_iterations)
+
+        layers = [nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)]
+        for name, module in resnet18().named_children():
+            if name == 'conv1' or isinstance(module, nn.MaxPool2d) or isinstance(module, nn.AvgPool2d) or isinstance(
+                    module, nn.Linear):
+                continue
+            layers.append(module)
+        self.features = nn.Sequential(*layers)
+        self.pool = nn.AvgPool2d(kernel_size=6)
+        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=128, out_capsules=10, in_length=4, out_length=8,
+                                                      routing_type='contract', share_weight=False,
+                                                      num_iterations=num_iterations))
 
     def forward(self, x):
         out = self.features(x)
-        res = out
-        out = self.bk1(out)
-        out += self.down1(res)
-        out = self.relu(out)
-        res = out
-        out = self.bk2(out)
-        out += self.down2(res)
-        out = self.relu(out)
-        res = out
-        out = self.bk3(out)
-        out += self.down3(res)
-        out = self.relu(out)
-        res = out
-        out = self.bk4(out)
-        out += self.down4(res)
-        out = self.relu(out)
+        out = self.pool(out)
 
         out = out.view(*out.size()[:2], -1)
         out = out.transpose(-1, -2)
-        out = out.contiguous().view(out.size(0), -1, 8)
+        out = out.contiguous().view(out.size(0), -1, 4)
 
         out = self.classifier(out)
         classes = out.norm(dim=-1)
