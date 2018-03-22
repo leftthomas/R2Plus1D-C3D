55ddf75e356ed38458f2e94c54363bf45a97a120
leftthomas <leftthomas@qq.com>
2018-03-22 16:28:29 +0800

    change model

--- models/mnist.py
+++ models/mnist.py
@@ -1,43 +1,31 @@
 from capsule_layer import CapsuleLinear
 from torch import nn
+from torchvision.models.resnet import resnet18
 
 
 class MNISTCapsuleNet(nn.Module):
     def __init__(self, num_iterations=3):
         super(MNISTCapsuleNet, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False),
-            nn.BatchNorm2d(num_features=64),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding=2, bias=False),
-            nn.BatchNorm2d(num_features=64),
-            nn.ReLU(inplace=True),
 
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=128),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=128),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(num_features=256),
-            nn.ReLU(inplace=True)
-
-        )
-        self.classifier = CapsuleLinear(in_capsules=512, out_capsules=10, in_length=8, out_length=16,
-                                        routing_type='dynamic', share_weight=False,
-                                        num_iterations=num_iterations)
+        layers = [nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)]
+        for name, module in resnet18().named_children():
+            if name == 'conv1' or isinstance(module, nn.MaxPool2d) or isinstance(module, nn.AvgPool2d) or isinstance(
+                    module, nn.Linear):
+                continue
+            layers.append(module)
+        self.features = nn.Sequential(*layers)
+        self.pool = nn.AvgPool2d(kernel_size=4)
+        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=128, out_capsules=10, in_length=4, out_length=8,
+                                                      routing_type='contract', share_weight=False,
+                                                      num_iterations=num_iterations))
 
     def forward(self, x):
         out = self.features(x)
+        out = self.pool(out)
 
         out = out.view(*out.size()[:2], -1)
         out = out.transpose(-1, -2)
-        out = out.contiguous().view(out.size(0), -1, 8)
+        out = out.contiguous().view(out.size(0), -1, 4)
 
         out = self.classifier(out)
         classes = out.norm(dim=-1)
