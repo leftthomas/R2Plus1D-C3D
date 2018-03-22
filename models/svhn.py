d2194c012370248e238d82522410e87dbf4b8ba2
leftthomas <leftthomas@qq.com>
2018-03-22 16:24:15 +0800

    change model

--- models/svhn.py
+++ models/svhn.py
@@ -1,34 +1,33 @@
-from capsule_layer import CapsuleConv2d, CapsuleLinear
+from capsule_layer import CapsuleLinear
 from torch import nn
+from torchvision.models.resnet import resnet18
 
 
 class SVHNCapsuleNet(nn.Module):
     def __init__(self, num_iterations=3):
         super(SVHNCapsuleNet, self).__init__()
-        self.features = nn.Sequential(
-            CapsuleConv2d(in_channels=3, out_channels=16, kernel_size=5, in_length=3, out_length=4, stride=1,
-                          padding=2),
-            CapsuleConv2d(in_channels=16, out_channels=16, kernel_size=3, in_length=4, out_length=4, stride=2,
-                          padding=1),
-            CapsuleConv2d(in_channels=16, out_channels=32, kernel_size=3, in_length=4, out_length=8, stride=1,
-                          padding=1),
-            CapsuleConv2d(in_channels=32, out_channels=32, kernel_size=3, in_length=8, out_length=8, stride=2,
-                          padding=1),
-            CapsuleConv2d(in_channels=32, out_channels=64, kernel_size=3, in_length=8, out_length=16, stride=1,
-                          padding=1),
-            CapsuleConv2d(in_channels=64, out_channels=64, kernel_size=3, in_length=16, out_length=16, stride=2,
-                          padding=1)
-        )
-        self.classifier = CapsuleLinear(in_capsules=64, out_capsules=10, in_length=16, out_length=32,
-                                        routing_type='dynamic', num_iterations=num_iterations)
+
+        layers = [nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)]
+        for name, module in resnet18().named_children():
+            if name == 'conv1' or isinstance(module, nn.MaxPool2d) or isinstance(module, nn.AvgPool2d) or isinstance(
+                    module, nn.Linear):
+                continue
+            layers.append(module)
+        self.features = nn.Sequential(*layers)
+        self.pool = nn.AvgPool2d(kernel_size=4)
+        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=128, out_capsules=10, in_length=4, out_length=8,
+                                                      routing_type='contract', share_weight=False,
+                                                      num_iterations=num_iterations))
 
     def forward(self, x):
         out = self.features(x)
+        out = self.pool(out)
 
         out = out.view(*out.size()[:2], -1)
         out = out.transpose(-1, -2)
-        out = out.contiguous().view(out.size(0), -1, 16)
+        out = out.contiguous().view(out.size(0), -1, 4)
 
         out = self.classifier(out)
         classes = out.norm(dim=-1)
         return classes
+
