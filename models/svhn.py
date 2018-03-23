e5634d65745557c71f858a082e950f77105264de
leftthomas <leftthomas@qq.com>
2018-03-24 03:33:36 +0800

    use preact_resnet44

--- models/svhn.py
+++ models/svhn.py
@@ -1,7 +1,7 @@
 from capsule_layer import CapsuleLinear
 from torch import nn
 
-from resnet import resnet44
+from resnet import preact_resnet44
 
 
 class SVHNCapsuleNet(nn.Module):
@@ -9,16 +9,16 @@ class SVHNCapsuleNet(nn.Module):
         super(SVHNCapsuleNet, self).__init__()
 
         layers = []
-        for name, module in resnet44().named_children():
+        for name, module in preact_resnet44().named_children():
             if isinstance(module, nn.AvgPool2d) or isinstance(module, nn.Linear):
                 continue
             layers.append(module)
         self.features = nn.Sequential(*layers)
         self.pool = nn.AvgPool2d(kernel_size=8)
-        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=32, out_capsules=24, in_length=2, out_length=4,
+        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=32, out_capsules=16, in_length=2, out_length=4,
                                                       routing_type='contract', share_weight=True,
                                                       num_iterations=num_iterations),
-                                        CapsuleLinear(in_capsules=24, out_capsules=10, in_length=4, out_length=8,
+                                        CapsuleLinear(in_capsules=16, out_capsules=10, in_length=4, out_length=8,
                                                       routing_type='contract', share_weight=False,
                                                       num_iterations=num_iterations))
 
