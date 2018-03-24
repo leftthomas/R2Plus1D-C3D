416b0aa0f28cf266645e45ffc3c4ce894ff34577
leftthomas <leftthomas@qq.com>
2018-03-24 14:26:16 +0800

    change out length to 16

--- models/svhn.py
+++ models/svhn.py
@@ -15,7 +15,7 @@ class SVHNCapsuleNet(nn.Module):
             layers.append(module)
         self.features = nn.Sequential(*layers)
         self.pool = nn.AvgPool2d(kernel_size=8)
-        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=32, out_capsules=10, in_length=2, out_length=8,
+        self.classifier = nn.Sequential(CapsuleLinear(in_capsules=32, out_capsules=10, in_length=2, out_length=16,
                                                       routing_type='contract', share_weight=True,
                                                       num_iterations=num_iterations))
 
