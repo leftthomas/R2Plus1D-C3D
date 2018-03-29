7bbd8daf1a2eb23ef178f66f6c23e9aef7fa9d3c
leftthomas <leftthomas@qq.com>
2018-03-29 21:01:26 +0800

    use share weight

--- models/cifar100.py
+++ models/cifar100.py
@@ -16,15 +16,15 @@ class CIFAR100CapsuleNet(nn.Module):
             layers.append(module)
         self.features = nn.Sequential(*layers)
         self.classifier = nn.Sequential(
-            CapsuleLinear(out_capsules=100, in_length=32, out_length=16, in_capsules=8 * 8 * 2,
-                          share_weight=False, routing_type='contract', num_iterations=num_iterations))
+            CapsuleLinear(out_capsules=100, in_length=8, out_length=16, in_capsules=None,
+                          share_weight=True, routing_type='contract', num_iterations=num_iterations))
 
     def forward(self, x):
         out = self.conv1(x)
         out = self.features(out)
 
         out = out.permute(0, 2, 3, 1)
-        out = out.contiguous().view(out.size(0), -1, 32)
+        out = out.contiguous().view(out.size(0), -1, 8)
 
         out = self.classifier(out)
         classes = out.sum(dim=-1)
