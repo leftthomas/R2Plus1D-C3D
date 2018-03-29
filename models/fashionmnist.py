7f214e346c8d884caedca915218edcbb83b0e343
leftthomas <leftthomas@qq.com>
2018-03-29 22:32:44 +0800

    change model

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -1,22 +1,16 @@
 from capsule_layer import CapsuleLinear
 from torch import nn
 
-from resnet import resnet20
-
 
 class FashionMNISTCapsuleNet(nn.Module):
     def __init__(self, num_iterations=3):
         super(FashionMNISTCapsuleNet, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1, bias=False)
-        layers = []
-        for name, module in resnet20().named_children():
-            if name == 'conv1' or isinstance(module, nn.AvgPool2d) or isinstance(module, nn.Linear):
-                continue
-            layers.append(module)
-        self.features = nn.Sequential(*layers)
-        self.classifier = nn.Sequential(CapsuleLinear(out_capsules=10, in_length=64, out_length=16, in_capsules=49,
-                                                      share_weight=False, routing_type='contract',
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=3, stride=2, padding=1), nn.ReLU())
+        self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+                                      nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1), nn.ReLU())
+        self.classifier = nn.Sequential(CapsuleLinear(out_capsules=10, in_length=128, out_length=16, in_capsules=None,
+                                                      share_weight=True, routing_type='contract',
                                                       num_iterations=num_iterations))
 
     def forward(self, x):
@@ -24,7 +18,7 @@ class FashionMNISTCapsuleNet(nn.Module):
         out = self.features(out)
 
         out = out.permute(0, 2, 3, 1)
-        out = out.contiguous().view(out.size(0), -1, 64)
+        out = out.contiguous().view(out.size(0), -1, 128)
 
         out = self.classifier(out)
         classes = out.norm(dim=-1)
