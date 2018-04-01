be838f866b5b1590d13d5baeda56131055b39ea5
leftthomas <leftthomas@qq.com>
2018-04-01 12:38:40 +0800

    change margin loss value to 0.5; remove lr scheduler

--- utils.py
+++ utils.py
@@ -71,7 +71,7 @@ class MarginLoss(nn.Module):
     def forward(self, classes, labels):
         left = F.relu(0.9 - classes, inplace=True) ** 2
         right = F.relu(classes - 0.1, inplace=True) ** 2
-        loss = labels * left + 0.25 * (1 - labels) * right
+        loss = labels * left + 0.5 * (1 - labels) * right
         return loss.mean()
 
 
