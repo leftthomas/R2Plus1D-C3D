8503cf7df422d51d2dfe9cf096ef6ba2b9fedfe5
leftthomas <leftthomas@qq.com>
2018-04-01 12:45:07 +0800

    test dynamic routing

--- models/mnist.py
+++ models/mnist.py
@@ -10,7 +10,7 @@ class MNISTNet(nn.Module):
         self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1), nn.ReLU(),
                                       nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(),
                                       nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1), nn.ReLU())
-        self.classifier = CapsuleLinear(out_capsules=10, in_length=32, out_length=16, in_capsules=7 * 7 * 4,
+        self.classifier = CapsuleLinear(out_capsules=10, in_length=128, out_length=16, in_capsules=7 * 7,
                                         share_weight=False, routing_type='dynamic', num_iterations=num_iterations)
 
     def forward(self, x):
@@ -18,7 +18,7 @@ class MNISTNet(nn.Module):
         out = self.features(out)
 
         out = out.permute(0, 2, 3, 1)
-        out = out.contiguous().view(out.size(0), -1, 32)
+        out = out.contiguous().view(out.size(0), -1, 128)
         out = self.classifier(out)
         classes = out.norm(dim=-1)
         return classes
