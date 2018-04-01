cceb7418c4914ebc983446d4d6d20192b9d32868
leftthomas <leftthomas@qq.com>
2018-04-01 18:44:25 +0800

    test dynamic routing

--- models/fashionmnist.py
+++ models/fashionmnist.py
@@ -6,12 +6,11 @@ class FashionMNISTNet(nn.Module):
     def __init__(self, num_iterations=3):
         super(FashionMNISTNet, self).__init__()
 
-        self.conv1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=3, padding=1), nn.ReLU())
-        self.features = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, padding=1), nn.ReLU(),
-                                      nn.AvgPool2d(kernel_size=2),
-                                      nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(),
-                                      nn.Conv2d(128, 128, kernel_size=3, padding=1), nn.ReLU())
-        self.classifier = CapsuleLinear(out_capsules=10, in_length=128, out_length=16, in_capsules=14 * 14,
+        self.conv1 = nn.Sequential(nn.Conv2d(1, 32, kernel_size=3, padding=1), nn.ReLU())
+        self.features = nn.Sequential(nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1), nn.ReLU(),
+                                      nn.Conv2d(32, 64, kernel_size=3, padding=1), nn.ReLU(),
+                                      nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1), nn.ReLU())
+        self.classifier = CapsuleLinear(out_capsules=10, in_length=64, out_length=16, in_capsules=7 * 7,
                                         share_weight=False, routing_type='dynamic', num_iterations=num_iterations)
 
     def forward(self, x):
@@ -19,7 +18,7 @@ class FashionMNISTNet(nn.Module):
         out = self.features(out)
 
         out = out.permute(0, 2, 3, 1)
-        out = out.contiguous().view(out.size(0), -1, 128)
+        out = out.contiguous().view(out.size(0), -1, 64)
         out = self.classifier(out)
         classes = out.norm(dim=-1)
         return classes
