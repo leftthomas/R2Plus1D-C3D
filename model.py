50372d874418060bfba7bfb25a70a758614ac87b
leftthomas <leftthomas@qq.com>
2019-01-26 17:46:32 +0800

    chang model

--- model.py
+++ model.py
@@ -2,26 +2,30 @@ import torch
 import torch.nn.functional as F
 from capsule_layer import CapsuleLinear
 from torch import nn
-from torch_geometric.nn import GCNConv
-from torch_scatter import scatter_mean
+from torch_geometric.nn import GCNConv, global_sort_pool
 
 
 class Model(nn.Module):
     def __init__(self, num_features, num_classes, num_iterations=3):
         super(Model, self).__init__()
 
-        self.conv1 = GCNConv(num_features, 16)
-        self.conv2 = GCNConv(16, num_classes)
-        self.classifier = CapsuleLinear(out_capsules=num_classes, in_length=128, out_length=32, in_capsules=None,
-                                        share_weight=True, routing_type='k_means', num_iterations=num_iterations)
+        self.conv1 = GCNConv(num_features, 32)
+        self.conv2 = GCNConv(32, 32)
+        self.conv3 = GCNConv(32, 32)
+        self.classifier = CapsuleLinear(out_capsules=num_classes, in_length=32, out_length=16, in_capsules=None,
+                                        share_weight=True, routing_type='k_means', similarity='tonimoto',
+                                        num_iterations=num_iterations)
 
     def forward(self, data):
-        x, edge_index = data.x, data.edge_index
+        x, edge_index, batch = data.x, data.edge_index, data.batch
 
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
-        x = scatter_mean(x, data.batch, dim=0)
+        x_1 = F.tanh(self.conv1(x, edge_index))
+        x_2 = F.tanh(self.conv2(x_1, edge_index))
+        x_3 = F.tanh(self.conv3(x_2, edge_index))
+        x = torch.cat([x_1, x_2, x_3], dim=-1)
+        x = global_sort_pool(x, batch, k=50)
+        x = x.view(x.size(0), 50 * 3, -1)
+        out = self.classifier(x)
+        classes = out.norm(dim=-1)
 
-        return torch.sigmoid(x)
+        return classes
