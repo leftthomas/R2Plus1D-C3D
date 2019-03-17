1e44fd329443b55f5eb300847314a5a31263c73c
leftthomas <leftthomas@qq.com>
2019-03-18 01:20:19 +0800

    make GCN is same

--- model.py
+++ model.py
@@ -13,7 +13,8 @@ class Model(nn.Module):
         self.conv1 = GCNConv(num_features, 32)
         self.conv2 = GCNConv(32, 32)
         self.conv3 = GCNConv(32, 32)
-        self.classifier = CapsuleLinear(out_capsules=num_classes, in_length=96, out_length=16, in_capsules=None,
+        self.conv4 = GCNConv(32, 1)
+        self.classifier = CapsuleLinear(out_capsules=num_classes, in_length=97, out_length=16, in_capsules=None,
                                         share_weight=True, routing_type='k_means', similarity='tonimoto',
                                         num_iterations=num_iterations)
 
@@ -23,7 +24,8 @@ class Model(nn.Module):
         x_1 = torch.tanh(self.conv1(x, edge_index))
         x_2 = torch.tanh(self.conv2(x_1, edge_index))
         x_3 = torch.tanh(self.conv3(x_2, edge_index))
-        x = torch.cat([x_1, x_2, x_3], dim=-1)
+        x_4 = torch.tanh(self.conv4(x_3, edge_index))
+        x = torch.cat([x_1, x_2, x_3, x_4], dim=-1)
         x = global_sort_pool(x, batch)
         out = self.classifier(x)
         classes = out.norm(dim=-1)
