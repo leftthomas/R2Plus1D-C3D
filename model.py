4b15b55112aa1df95eeea6f4b8bcc2c26a6530e3
leftthomas <leftthomas@qq.com>
2019-03-22 17:46:18 +0800

    use 3 hop GCN

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 from capsule_layer import CapsuleLinear
 from torch import nn
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import SGConv
 from torch_geometric.utils import remove_self_loops
 
 from utils import global_sort_pool
@@ -11,9 +11,9 @@ class Model(nn.Module):
     def __init__(self, num_features, num_classes, num_iterations=3):
         super(Model, self).__init__()
 
-        self.gcn1 = GCNConv(num_features, 32)
-        self.gcn2 = GCNConv(32, 32)
-        self.gcn3 = GCNConv(32, 32)
+        self.gcn1 = SGConv(num_features, 32, K=3)
+        self.gcn2 = SGConv(32, 32, K=3)
+        self.gcn3 = SGConv(32, 32, K=3)
         self.classifier = CapsuleLinear(out_capsules=num_classes, in_length=96, out_length=32, in_capsules=50,
                                         share_weight=False, num_iterations=num_iterations)
 
@@ -23,7 +23,7 @@ class Model(nn.Module):
 
         x_1 = torch.tanh(self.gcn1(x, edge_index))
         x_2 = torch.tanh(self.gcn2(x_1, edge_index))
-        x_3 = torch.tanh(self.gcn3(x_2 + x_1, edge_index))
+        x_3 = torch.tanh(self.gcn3(x_2, edge_index))
         x = torch.cat([x_1, x_2, x_3], dim=-1)
         out = self.classifier(global_sort_pool(x, batch, k=50))
         classes = out.norm(dim=-1)
