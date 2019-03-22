4b084a1a8d322649476607219fc06823682afa87
leftthomas <leftthomas@qq.com>
2019-03-22 17:53:10 +0800

    use two CapsuleLinear

--- model.py
+++ model.py
@@ -14,8 +14,10 @@ class Model(nn.Module):
         self.gcn1 = GCNConv(num_features, 32)
         self.gcn2 = GCNConv(32, 32)
         self.gcn3 = GCNConv(32, 32)
-        self.classifier = CapsuleLinear(out_capsules=num_classes, in_length=96, out_length=32, in_capsules=50,
-                                        share_weight=False, num_iterations=num_iterations)
+        self.classifier_1 = CapsuleLinear(out_capsules=16, in_length=96, out_length=32, in_capsules=50,
+                                          share_weight=False, num_iterations=num_iterations)
+        self.classifier_2 = CapsuleLinear(out_capsules=num_classes, in_length=32, out_length=16, in_capsules=16,
+                                          share_weight=False, num_iterations=num_iterations)
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
@@ -25,7 +27,8 @@ class Model(nn.Module):
         x_2 = torch.tanh(self.gcn2(x_1, edge_index))
         x_3 = torch.tanh(self.gcn3(x_2, edge_index))
         x = torch.cat([x_1, x_2, x_3], dim=-1)
-        out = self.classifier(global_sort_pool(x, batch, k=50))
+        out = self.classifier_1(global_sort_pool(x, batch, k=50))
+        out = self.classifier_2(out)
         classes = out.norm(dim=-1)
 
         return classes
