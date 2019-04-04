b54d796b304b42bf374d28b00a837595a9149b4d
leftthomas <leftthomas@qq.com>
2019-04-05 01:25:57 +0800

    add preprocess to raw data

--- README.md
+++ README.md
@@ -11,6 +11,10 @@ conda install pytorch torchvision -c pytorch
 ```
 conda install opencv
 ```
+- rarfile
+```
+pip install rarfile
+```
 - PyTorchNet
 ```
 pip install git+https://github.com/pytorch/tnt.git@master
@@ -22,16 +26,18 @@ pip install git+https://github.com/leftthomas/CapsuleLayer.git@master
 
 ## Datasets
 The datasets are coming from [UCF101](http://crcv.ucf.edu/data/UCF101.php)
-and [HMDB51](http://serre-lab.clps.brown.edu/resource/hmdb-a-large-human-motion-database/).
-Download these datasets and then extract them into `data` directory. 
-Rename `UCF-101` directory as `ucf101`, `hmdb-51` directory as `hmdb51`.
+、[HMDB51](http://serre-lab.clps.brown.edu/resource/hmdb-a-large-human-motion-database/) 
+and [20BN-something-something](https://20bn.com/datasets/something-something).
+Download these datasets and `train/val/test` split files into `data` directory.
+
+Run `misc.py` to preprocess these datasets.
 
 ## Usage
 ### Train Model
 ```
 visdom -logging_level WARNING & python train.py --num_epochs 200
 optional arguments:
---data_type                   dataset type [default value is 'ucf101'](choices=['ucf101', 'hmdb51'])
+--data_type                   dataset type [default value is 'ucf101'](choices=['ucf101', 'hmdb51', 'ss174'])
 --clip_len                    number of frames in each video [default value is 16]
 --batch_size                  training batch size [default value is 20]
 --num_epochs                  training epochs number [default value is 100]
@@ -42,7 +48,7 @@ Visdom now can be accessed by going to `127.0.0.1:8097` in your browser.
 ```
 python inference.py --video_name data/ucf101/ApplyLipstick/v_ApplyLipstick_g04_c02.avi
 optional arguments:
---data_type                   dataset type [default value is 'ucf101'](choices=['ucf101', 'hmdb51'])
+--data_type                   dataset type [default value is 'ucf101'](choices=['ucf101', 'hmdb51', 'ss174'])
 --clip_len                    number of frames in each video [default value is 16]
 --video_name                  test video name
 --model_name                  model epoch name [default value is 'ucf101_100.pth']
@@ -51,6 +57,7 @@ The inferences will show in a pop up window.
 
 ## Results
 The train/val/test loss、accuracy and confusion matrix are showed on visdom.
-![result](results/mutag.png)
-![result](results/ptc.png)
+![result](results/ucf101.png)
+![result](results/hmdb51.png)
+![result](results/ss174.png)
 
---
+++ misc.py
@@ -0,0 +1,100 @@
+import os
+import shutil
+import zipfile
+
+import rarfile
+from sklearn.model_selection import train_test_split
+
+if not os.path.exists('data/temp'):
+    os.mkdir('data/temp')
+
+# preprocess ucf101 files
+if not os.path.exists('data/ucf101'):
+    os.mkdir('data/ucf101')
+if not os.path.exists('data/temp/ucf101'):
+    os.mkdir('data/temp/ucf101')
+
+ucf101_splits = zipfile.ZipFile('data/UCF101TrainTestSplits-RecognitionTask.zip')
+ucf101_splits.extractall('data/temp/ucf101')
+ucf101_splits.close()
+
+if not os.path.exists('data/ucf101/labels.txt'):
+    with open('data/ucf101/labels.txt', 'w') as f:
+        for line in open('data/temp/ucf101/ucfTrainTestlist/classInd.txt', 'r'):
+            f.write(line.split(' ')[1])
+
+train_video_files, val_video_files, test_video_files, train_video_labels = [], [], [], []
+for line in open('data/temp/ucf101/ucfTrainTestlist/trainlist01.txt', 'r'):
+    train_video_files.append(line.split(' ')[0])
+    train_video_labels.append(line.split(' ')[1].replace('\n', ''))
+for line in open('data/temp/ucf101/ucfTrainTestlist/testlist01.txt', 'r'):
+    test_video_files.append(line.replace('\n', ''))
+train_video_files, val_video_files, _, _ = train_test_split(train_video_files, train_video_labels, test_size=0.2,
+                                                            random_state=42)
+
+ucf101_videos = rarfile.RarFile('data/UCF101.rar')
+ucf101_videos.extractall('data/temp/ucf101')
+ucf101_videos.close()
+
+if not os.path.exists('data/ucf101/train'):
+    os.mkdir('data/ucf101/train')
+for video in train_video_files:
+    if not os.path.exists('data/ucf101/train/{}'.format(video.split('/')[0])):
+        os.mkdir('data/ucf101/train/{}'.format(video.split('/')[0]))
+    shutil.move('data/temp/ucf101/UCF-101/{}'.format(video), 'data/ucf101/train/{}'.format(video))
+
+if not os.path.exists('data/ucf101/val'):
+    os.mkdir('data/ucf101/val')
+for video in val_video_files:
+    if not os.path.exists('data/ucf101/val/{}'.format(video.split('/')[0])):
+        os.mkdir('data/ucf101/val/{}'.format(video.split('/')[0]))
+    shutil.move('data/temp/ucf101/UCF-101/{}'.format(video), 'data/ucf101/val/{}'.format(video))
+
+if not os.path.exists('data/ucf101/test'):
+    os.mkdir('data/ucf101/test')
+for video in test_video_files:
+    if not os.path.exists('data/ucf101/test/{}'.format(video.split('/')[0])):
+        os.mkdir('data/ucf101/test/{}'.format(video.split('/')[0]))
+    shutil.move('data/temp/ucf101/UCF-101/{}'.format(video), 'data/ucf101/test/{}'.format(video))
+
+# preprocess hmdb51 files
+if not os.path.exists('data/hmdb51'):
+    os.mkdir('data/hmdb51')
+if not os.path.exists('data/temp/hmdb51'):
+    os.mkdir('data/temp/hmdb51')
+
+hmdb51_splits = rarfile.RarFile('data/test_train_splits.rar')
+hmdb51_splits.extractall('data/temp/hmdb51')
+hmdb51_splits.close()
+
+labels = []
+for file in os.listdir('data/temp/hmdb51/testTrainMulti_7030_splits'):
+    labels.append(file.split('_')[0])
+labels = set(labels)
+
+if not os.path.exists('data/hmdb51/labels.txt'):
+    with open('data/hmdb51/labels.txt', 'w') as f:
+        for label in labels:
+            f.write(label + '\n')
+
+train_video_files, val_video_files, test_video_files = [], [], []
+for file in os.listdir('data/temp/hmdb51/testTrainMulti_7030_splits'):
+    if file.endswith('test_split1.txt'):
+        for line in open('data/temp/hmdb51/testTrainMulti_7030_splits/{}'.format(file), 'r'):
+            if line.split(' ')[1].replace('\n', '') == '1':
+                train_video_files.append(line.split(' ')[0])
+            if line.split(' ')[1].replace('\n', '') == '2':
+                test_video_files.append(line.split(' ')[0])
+            if line.split(' ')[1].replace('\n', '') == '0':
+                val_video_files.append(line.split(' ')[0])
+
+hmdb51_videos = rarfile.RarFile('data/hmdb51_org.rar')
+hmdb51_videos.extractall('data/temp/hmdb51')
+hmdb51_videos.close()
+for file in os.listdir('data/temp/hmdb51/'):
+    if file.endswith('.rar'):
+        rar_file = rarfile.RarFile('data/temp/hmdb51/{}'.format(file))
+        rar_file.extractall('data/temp/hmdb51')
+        rar_file.close()
+
+# os.remove(file_name)
