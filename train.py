8bfa39a94d3d931aa427c07ce5c24751b754e6bb
leftthomas <leftthomas@qq.com>
2019-04-05 21:24:49 +0800

    add top5 accuracy

--- train.py
+++ train.py
@@ -48,12 +48,14 @@ def on_start_epoch(state):
 
 def on_end_epoch(state):
     loss_logger.log(state['epoch'], meter_loss.value()[0], name='train')
-    accuracy_logger.log(state['epoch'], meter_accuracy.value()[0], name='train')
+    top1_accuracy_logger.log(state['epoch'], meter_accuracy.value()[0], name='train')
+    top5_accuracy_logger.log(state['epoch'], meter_accuracy.value()[1], name='train')
     train_confusion_logger.log(meter_confusion.value())
     results['train_loss'].append(meter_loss.value()[0])
-    results['train_accuracy'].append(meter_accuracy.value()[0])
-    print('[Epoch %d] Training Loss: %.4f Accuracy: %.2f%%' % (
-        state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0]))
+    results['train_top1_accuracy'].append(meter_accuracy.value()[0])
+    results['train_top5_accuracy'].append(meter_accuracy.value()[1])
+    print('[Epoch %d] Training Loss: %.4f Top1 Accuracy: %.2f%% Top5 Accuracy: %.2f%%' % (
+        state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0], meter_accuracy.value()[1]))
 
     reset_meters()
 
@@ -61,12 +63,14 @@ def on_end_epoch(state):
         engine.test(processor, val_loader)
 
     loss_logger.log(state['epoch'], meter_loss.value()[0], name='val')
-    accuracy_logger.log(state['epoch'], meter_accuracy.value()[0], name='val')
+    top1_accuracy_logger.log(state['epoch'], meter_accuracy.value()[0], name='val')
+    top5_accuracy_logger.log(state['epoch'], meter_accuracy.value()[1], name='val')
     val_confusion_logger.log(meter_confusion.value())
     results['val_loss'].append(meter_loss.value()[0])
-    results['val_accuracy'].append(meter_accuracy.value()[0])
-    print('[Epoch %d] Valing Loss: %.4f Accuracy: %.2f%%' % (
-        state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0]))
+    results['val_top1_accuracy'].append(meter_accuracy.value()[0])
+    results['val_top5_accuracy'].append(meter_accuracy.value()[1])
+    print('[Epoch %d] Valing Loss: %.4f Top1 Accuracy: %.2f%% Top5 Accuracy: %.2f%%' % (
+        state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0], meter_accuracy.value()[1]))
 
     # save best model
     global best_accuracy
@@ -81,19 +85,23 @@ def on_end_epoch(state):
         engine.test(processor, test_loader)
 
     loss_logger.log(state['epoch'], meter_loss.value()[0], name='test')
-    accuracy_logger.log(state['epoch'], meter_accuracy.value()[0], name='test')
+    top1_accuracy_logger.log(state['epoch'], meter_accuracy.value()[0], name='test')
+    top5_accuracy_logger.log(state['epoch'], meter_accuracy.value()[1], name='test')
     test_confusion_logger.log(meter_confusion.value())
     results['test_loss'].append(meter_loss.value()[0])
-    results['test_accuracy'].append(meter_accuracy.value()[0])
-    print('[Epoch %d] Testing Loss: %.4f Accuracy: %.2f%%' % (
-        state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0]))
+    results['test_top1_accuracy'].append(meter_accuracy.value()[0])
+    results['test_top5_accuracy'].append(meter_accuracy.value()[1])
+    print('[Epoch %d] Testing Loss: %.4f Top1 Accuracy: %.2f%% Top5 Accuracy: %.2f%%' % (
+        state['epoch'], meter_loss.value()[0], meter_accuracy.value()[0], meter_accuracy.value()[1]))
 
     # save statistics at every 10 epochs
     if state['epoch'] % 10 == 0:
         data_frame = pd.DataFrame(
-            data={'train_loss': results['train_loss'], 'train_accuracy': results['train_accuracy'],
-                  'val_loss': results['val_loss'], 'val_accuracy': results['val_accuracy'],
-                  'test_loss': results['test_loss'], 'test_accuracy': results['test_accuracy']},
+            data={'train_loss': results['train_loss'], 'train_top1_accuracy': results['train_top1_accuracy'],
+                  'train_top5_accuracy': results['train_top5_accuracy'], 'val_loss': results['val_loss'],
+                  'val_top1_accuracy': results['val_top1_accuracy'], 'val_top5_accuracy': results['val_top5_accuracy'],
+                  'test_loss': results['test_loss'], 'test_top1_accuracy': results['test_top1_accuracy'],
+                  'test_top5_accuracy': results['test_top5_accuracy']},
             index=range(1, state['epoch'] + 1))
         data_frame.to_csv('statistics/{}_results.csv'.format(DATA_TYPE), index_label='epoch')
 
@@ -113,8 +121,9 @@ if __name__ == '__main__':
     NUM_EPOCH = opt.num_epochs
 
     DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    results = {'train_loss': [], 'train_accuracy': [], 'val_loss': [], 'val_accuracy': [], 'test_loss': [],
-               'test_accuracy': []}
+    results = {'train_loss': [], 'train_top1_accuracy': [], 'train_top5_accuracy': [], 'val_loss': [],
+               'val_top1_accuracy': [], 'val_top5_accuracy': [], 'test_loss': [], 'test_top1_accuracy': [],
+               'test_top5_accuracy': []}
     # record best val accuracy
     best_accuracy = 0
 
@@ -128,11 +137,12 @@ if __name__ == '__main__':
 
     engine = Engine()
     meter_loss = tnt.meter.AverageValueMeter()
-    meter_accuracy = tnt.meter.ClassErrorMeter(accuracy=True)
+    meter_accuracy = tnt.meter.ClassErrorMeter(topk=[1, 5], accuracy=True)
     meter_confusion = tnt.meter.ConfusionMeter(NUM_CLASS, normalized=True)
 
     loss_logger = VisdomPlotLogger('line', env=DATA_TYPE, opts={'title': 'Loss'})
-    accuracy_logger = VisdomPlotLogger('line', env=DATA_TYPE, opts={'title': 'Accuracy'})
+    top1_accuracy_logger = VisdomPlotLogger('line', env=DATA_TYPE, opts={'title': 'Top1 Accuracy'})
+    top5_accuracy_logger = VisdomPlotLogger('line', env=DATA_TYPE, opts={'title': 'Top5 Accuracy'})
     train_confusion_logger = VisdomLogger('heatmap', env=DATA_TYPE, opts={'title': 'Train Confusion Matrix'})
     val_confusion_logger = VisdomLogger('heatmap', env=DATA_TYPE, opts={'title': 'Val Confusion Matrix'})
     test_confusion_logger = VisdomLogger('heatmap', env=DATA_TYPE, opts={'title': 'Test Confusion Matrix'})
