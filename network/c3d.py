d08040b8af219b648b2dd027c37db0da69cdf23f
leftthomas <leftthomas@qq.com>
2019-04-06 05:00:11 +0800

    add BN module

--- network/c3d.py
+++ network/c3d.py
@@ -6,21 +6,29 @@ class C3D(nn.Module):
         super(C3D, self).__init__()
 
         self.conv1 = nn.Conv3d(3, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.bn1 = nn.BatchNorm3d(num_features=64)
         self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
 
         self.conv2 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.bn2 = nn.BatchNorm3d(num_features=128)
         self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
         self.conv3a = nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.bn3a = nn.BatchNorm3d(num_features=256)
         self.conv3b = nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.bn3b = nn.BatchNorm3d(num_features=256)
         self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
         self.conv4a = nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.bn4a = nn.BatchNorm3d(num_features=512)
         self.conv4b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.bn4b = nn.BatchNorm3d(num_features=512)
         self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
         self.conv5a = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.bn5a = nn.BatchNorm3d(num_features=512)
         self.conv5b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.bn5b = nn.BatchNorm3d(num_features=512)
         self.pool5 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 1, 1))
 
         self.fc6 = nn.Linear(8192, 4096)
@@ -35,22 +43,22 @@ class C3D(nn.Module):
 
     def forward(self, x):
 
-        x = self.relu(self.conv1(x))
+        x = self.relu(self.bn1(self.conv1(x)))
         x = self.pool1(x)
 
-        x = self.relu(self.conv2(x))
+        x = self.relu(self.bn2(self.conv2(x)))
         x = self.pool2(x)
 
-        x = self.relu(self.conv3a(x))
-        x = self.relu(self.conv3b(x))
+        x = self.relu(self.bn3a(self.conv3a(x)))
+        x = self.relu(self.bn3b(self.conv3b(x)))
         x = self.pool3(x)
 
-        x = self.relu(self.conv4a(x))
-        x = self.relu(self.conv4b(x))
+        x = self.relu(self.bn4a(self.conv4a(x)))
+        x = self.relu(self.bn4b(self.conv4b(x)))
         x = self.pool4(x)
 
-        x = self.relu(self.conv5a(x))
-        x = self.relu(self.conv5b(x))
+        x = self.relu(self.bn5a(self.conv5a(x)))
+        x = self.relu(self.bn5b(self.conv5b(x)))
         x = self.pool5(x)
 
         x = x.view(-1, 8192)
