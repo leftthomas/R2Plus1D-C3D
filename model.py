af7a64f293c530378425381ab6e1c2882b27dcb0
leftthomas <leftthomas@qq.com>
2019-04-08 21:46:42 +0800

    finish two stream

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import math
 
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.nn.modules.utils import _triple
 
 
@@ -123,65 +124,97 @@ class Model(nn.Module):
     def __init__(self, num_classes):
         super(Model, self).__init__()
 
-        self.conv1 = nn.Conv3d(3, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.bn1 = nn.BatchNorm3d(num_features=64)
-        self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        # SpatioTemporal Stream
+        self.conv1a = SpatioTemporalConv(3, 96, kernel_size=(7, 7, 7), stride=2, padding=(3, 3, 3))
+        self.bn1a = nn.BatchNorm3d(num_features=96)
+        self.pool1a = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
-        self.conv2 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.bn2 = nn.BatchNorm3d(num_features=128)
-        self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        self.conv2a = SpatioTemporalConv(96, 256, kernel_size=(5, 5, 5), stride=2, padding=(2, 2, 2))
+        self.bn2a = nn.BatchNorm3d(num_features=256)
+        self.pool2a = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
-        self.conv3a = nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.bn3a = nn.BatchNorm3d(num_features=256)
-        self.conv3b = nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.bn3b = nn.BatchNorm3d(num_features=256)
-        self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        self.conv3a = SpatioTemporalConv(256, 512, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1))
+        self.bn3a = nn.BatchNorm3d(num_features=512)
 
-        self.conv4a = nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv4a = SpatioTemporalConv(512, 512, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1))
         self.bn4a = nn.BatchNorm3d(num_features=512)
-        self.conv4b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.bn4b = nn.BatchNorm3d(num_features=512)
-        self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
 
-        self.conv5a = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv5a = SpatioTemporalConv(512, 512, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1))
         self.bn5a = nn.BatchNorm3d(num_features=512)
-        self.conv5b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.pool5a = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(1, 1, 1))
+
+        self.fc6a = nn.Linear(8192, 4096)
+        self.fc7a = nn.Linear(4096, 2048)
+        self.fc8a = nn.Linear(2048, num_classes)
+
+        # TemporalSpatio Stream
+        self.conv1b = TemporalSpatioConv(3, 96, kernel_size=(7, 7, 7), stride=2, padding=(3, 3, 3))
+        self.bn1b = nn.BatchNorm3d(num_features=96)
+        self.pool1b = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+
+        self.conv2b = TemporalSpatioConv(96, 256, kernel_size=(5, 5, 5), stride=2, padding=(2, 2, 2))
+        self.bn2b = nn.BatchNorm3d(num_features=256)
+        self.pool2b = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+
+        self.conv3b = TemporalSpatioConv(256, 512, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1))
+        self.bn3b = nn.BatchNorm3d(num_features=512)
+
+        self.conv4b = TemporalSpatioConv(512, 512, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1))
+        self.bn4b = nn.BatchNorm3d(num_features=512)
+
+        self.conv5b = TemporalSpatioConv(512, 512, kernel_size=(3, 3, 3), stride=1, padding=(1, 1, 1))
         self.bn5b = nn.BatchNorm3d(num_features=512)
-        self.pool5 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 1, 1))
+        self.pool5b = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(1, 1, 1))
 
-        self.fc6 = nn.Linear(8192, 4096)
-        self.fc7 = nn.Linear(4096, 4096)
-        self.fc8 = nn.Linear(4096, num_classes)
+        self.fc6b = nn.Linear(8192, 4096)
+        self.fc7b = nn.Linear(4096, 2048)
+        self.fc8b = nn.Linear(2048, num_classes)
 
+        # common modules
         self.dropout = nn.Dropout(p=0.5)
-
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        x = self.relu(self.bn1(self.conv1(x)))
-        x = self.pool1(x)
+        # SpatioTemporal pipeline
+        x_a = self.relu(self.bn1a(self.conv1a(x)))
+        x_a = self.pool1a(x_a)
 
-        x = self.relu(self.bn2(self.conv2(x)))
-        x = self.pool2(x)
+        x_a = self.relu(self.bn2a(self.conv2a(x_a)))
+        x_a = self.pool2a(x_a)
 
-        x = self.relu(self.bn3a(self.conv3a(x)))
-        x = self.relu(self.bn3b(self.conv3b(x)))
-        x = self.pool3(x)
+        x_a = self.relu(self.bn3a(self.conv3a(x_a)))
+        x_a = self.relu(self.bn4a(self.conv4a(x_a)))
 
-        x = self.relu(self.bn4a(self.conv4a(x)))
-        x = self.relu(self.bn4b(self.conv4b(x)))
-        x = self.pool4(x)
+        x_a = self.relu(self.bn5a(self.conv5a(x_a)))
+        x_a = self.pool5a(x_a)
 
-        x = self.relu(self.bn5a(self.conv5a(x)))
-        x = self.relu(self.bn5b(self.conv5b(x)))
-        x = self.pool5(x)
+        x_a = x_a.view(-1, 8192)
+        x_a = self.relu(self.fc6a(x_a))
+        x_a = self.dropout(x_a)
+        x_a = self.relu(self.fc7a(x_a))
+        x_a = self.dropout(x_a)
+        logits_a = self.fc8a(x_a)
 
-        x = x.view(-1, 8192)
-        x = self.relu(self.fc6(x))
-        x = self.dropout(x)
-        x = self.relu(self.fc7(x))
-        x = self.dropout(x)
+        # TemporalSpatio pipeline
+        x_b = self.relu(self.bn1b(self.conv1b(x)))
+        x_b = self.pool1b(x_b)
 
-        logits = self.fc8(x)
+        x_b = self.relu(self.bn2b(self.conv2b(x_b)))
+        x_b = self.pool2b(x_b)
+
+        x_b = self.relu(self.bn3b(self.conv3b(x_b)))
+        x_b = self.relu(self.bn4b(self.conv4b(x_b)))
+
+        x_b = self.relu(self.bn5b(self.conv5b(x_b)))
+        x_b = self.pool5b(x_b)
+
+        x_b = x_b.view(-1, 8192)
+        x_b = self.relu(self.fc6b(x_b))
+        x_b = self.dropout(x_b)
+        x_b = self.relu(self.fc7b(x_b))
+        x_b = self.dropout(x_b)
+        logits_b = self.fc8b(x_b)
+
+        logits = F.softmax(logits_a + logits_b, dim=-1)
 
         return logits
