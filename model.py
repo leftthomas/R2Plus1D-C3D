238fe621e69ebd50ac9515694ebc2e3c4838fe35
leftthomas <leftthomas@qq.com>
2019-04-08 21:12:05 +0800

    add SpatioTemporalConv and TemporalSpatioConv

--- model.py
+++ model.py
@@ -1,4 +1,122 @@
+import math
+
 import torch.nn as nn
+from torch.nn.modules.utils import _triple
+
+
+class SpatioTemporalConv(nn.Module):
+    r"""Applies a factored 3D convolution over an input signal composed of several input
+    planes with distinct spatial and time axes, by performing a 2D convolution over the
+    spatial axes to an intermediate subspace, followed by a 1D convolution over the time
+    axis to produce the final output.
+    Args:
+        in_channels (int): Number of channels in the input tensor
+        out_channels (int): Number of channels produced by the convolution
+        kernel_size (int or tuple): Size of the convolving kernel
+        stride (int or tuple, optional): Stride of the convolution. Default: 1
+        padding (int or tuple, optional): Zero-padding added to the sides of the input during
+              their respective convolutions. Default: 0
+        bias (bool, optional): If ``True``, adds a learnable bias to the output. Default: ``True``
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=True):
+        super(SpatioTemporalConv, self).__init__()
+
+        kernel_size = _triple(kernel_size)
+        stride = _triple(stride)
+        padding = _triple(padding)
+        # decomposing the parameters into spatial and temporal components by
+        # masking out the values with the defaults on the axis that
+        # won't be convolved over.
+        spatial_kernel_size = (1, kernel_size[1], kernel_size[2])
+        spatial_stride = (1, stride[1], stride[2])
+        spatial_padding = (0, padding[1], padding[2])
+
+        temporal_kernel_size = (kernel_size[0], 1, 1)
+        temporal_stride = (stride[0], 1, 1)
+        temporal_padding = (padding[0], 0, 0)
+
+        # compute the number of intermediary channels (M)
+        if bias is True:
+            intermed_channels = int(math.floor(
+                (kernel_size[0] * kernel_size[1] * kernel_size[2] * in_channels * out_channels) / (
+                        1 + kernel_size[1] * kernel_size[2] * in_channels + kernel_size[0] * out_channels)))
+        else:
+            intermed_channels = int(math.floor(
+                (kernel_size[0] * kernel_size[1] * kernel_size[2] * in_channels * out_channels) / (
+                        kernel_size[1] * kernel_size[2] * in_channels + kernel_size[0] * out_channels)))
+
+        self.spatial_conv = nn.Conv3d(in_channels, intermed_channels, spatial_kernel_size,
+                                      stride=spatial_stride, padding=spatial_padding, bias=bias)
+        self.bn1 = nn.BatchNorm3d(intermed_channels)
+
+        self.temporal_conv = nn.Conv3d(intermed_channels, out_channels, temporal_kernel_size,
+                                       stride=temporal_stride, padding=temporal_padding, bias=bias)
+        self.bn2 = nn.BatchNorm3d(out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.spatial_conv(x)))
+        x = self.relu(self.bn2(self.temporal_conv(x)))
+        return x
+
+
+class TemporalSpatioConv(nn.Module):
+    r"""Applies a factored 3D convolution over an input signal composed of several input
+    planes with distinct spatial and time axes, by performing a 1D convolution over the time
+    axis to an intermediate subspace, followed by a 2D convolution over the spatial axes to
+    produce the final output.
+    Args:
+        in_channels (int): Number of channels in the input tensor
+        out_channels (int): Number of channels produced by the convolution
+        kernel_size (int or tuple): Size of the convolving kernel
+        stride (int or tuple, optional): Stride of the convolution. Default: 1
+        padding (int or tuple, optional): Zero-padding added to the sides of the input during
+              their respective convolutions. Default: 0
+        bias (bool, optional): If ``True``, adds a learnable bias to the output. Default: ``True``
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=True):
+        super(TemporalSpatioConv, self).__init__()
+
+        kernel_size = _triple(kernel_size)
+        stride = _triple(stride)
+        padding = _triple(padding)
+        # decomposing the parameters into temporal and spatial components by
+        # masking out the values with the defaults on the axis that
+        # won't be convolved over.
+        temporal_kernel_size = (kernel_size[0], 1, 1)
+        temporal_stride = (stride[0], 1, 1)
+        temporal_padding = (padding[0], 0, 0)
+
+        spatial_kernel_size = (1, kernel_size[1], kernel_size[2])
+        spatial_stride = (1, stride[1], stride[2])
+        spatial_padding = (0, padding[1], padding[2])
+
+        # compute the number of intermediary channels (M)
+        if bias is True:
+            intermed_channels = int(math.floor(
+                (kernel_size[0] * kernel_size[1] * kernel_size[2] * in_channels * out_channels) / (
+                        1 + kernel_size[1] * kernel_size[2] * out_channels + kernel_size[0] * in_channels)))
+        else:
+            intermed_channels = int(math.floor(
+                (kernel_size[0] * kernel_size[1] * kernel_size[2] * in_channels * out_channels) / (
+                        kernel_size[1] * kernel_size[2] * out_channels + kernel_size[0] * in_channels)))
+
+        self.temporal_conv = nn.Conv3d(in_channels, intermed_channels, temporal_kernel_size,
+                                       stride=temporal_stride, padding=temporal_padding, bias=bias)
+        self.bn1 = nn.BatchNorm3d(intermed_channels)
+
+        self.spatial_conv = nn.Conv3d(intermed_channels, out_channels, spatial_kernel_size,
+                                      stride=spatial_stride, padding=spatial_padding, bias=bias)
+
+        self.bn2 = nn.BatchNorm3d(out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.temporal_conv(x)))
+        x = self.relu(self.bn2(self.spatial_conv(x)))
+        return x
 
 
 class Model(nn.Module):
