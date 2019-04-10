change resize height and fix bug about the computation of resize size and clip length

utils.py
@@ -6,8 +6,6 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-MIN_FRAME, MIN_WIDTH, MIN_HEIGHT = 1000, 1000, 1000
-
 
 class VideoDataset(Dataset):
     r"""A Dataset for a folder of videos. Expects the directory structure to be
@@ -22,13 +20,9 @@ class VideoDataset(Dataset):
     def __init__(self, dataset='ucf101', split='train'):
         self.original_dir = os.path.join('data', dataset)
         self.preprocessed_dir = os.path.join('data', 'preprocessed_' + dataset)
-        # min shape: (frame, height, width)
-        # ucf101 min shape: (19, 240, 176)
-        # hmdb51 min shape: (19, 240, 176)
-        # kinetics600 min shape: (19, 240, 176)
         self.split = split
         self.clip_len = 16
-        self.resize_height = 120
+        self.resize_height = 128
         self.crop_size = 112
 
         if not self.check_integrity():
@@ -93,7 +87,6 @@ class VideoDataset(Dataset):
                 self.process_video(video_name, save_name)
 
         print('Preprocessing finished.')
-        print('min frame:{}; min height:{}; min width:{}'.format(str(MIN_FRAME), str(MIN_HEIGHT), str(MIN_WIDTH)))
 
     def process_video(self, video_name, save_name):
         # initialize a VideoCapture object to read video data into a numpy array
@@ -102,23 +95,13 @@ class VideoDataset(Dataset):
         frame_height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
         frame_width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 
-        global MIN_FRAME
-        if frame_count < MIN_FRAME:
-            MIN_FRAME = frame_count
-        global MIN_HEIGHT
-        if frame_height < MIN_HEIGHT:
-            MIN_HEIGHT = frame_height
-        global MIN_WIDTH
-        if frame_width < MIN_WIDTH:
-            MIN_WIDTH = frame_width
-
-        # make sure the preprocessed video has at least 16 frames
+        # make sure the preprocessed video has at least clip_len frames
         extract_frequency = 4
-        if frame_count // extract_frequency <= 16:
+        if frame_count // extract_frequency <= self.clip_len:
             extract_frequency -= 1
-            if frame_count // extract_frequency <= 16:
+            if frame_count // extract_frequency <= self.clip_len:
                 extract_frequency -= 1
-                if frame_count // extract_frequency <= 16:
+                if frame_count // extract_frequency <= self.clip_len:
                     extract_frequency -= 1
 
         count = 0
@@ -131,8 +114,14 @@ class VideoDataset(Dataset):
                 continue
 
             if count % extract_frequency == 0:
-                resize_width = math.floor(frame_width / frame_height * self.resize_height)
-                frame = cv2.resize(frame, (resize_width, self.resize_height))
+                resize_height = self.resize_height
+                resize_width = math.floor(frame_width / frame_height * resize_height)
+                # make sure resize width >= crop size
+                if resize_width < self.crop_size:
+                    resize_width = self.resize_height
+                    resize_height = math.floor(frame_height / frame_width * resize_width)
+
+                frame = cv2.resize(frame, (resize_width, resize_height))
                 if not os.path.exists(save_name.split('.')[0]):
                     os.mkdir(save_name.split('.')[0])
                 cv2.imwrite(filename=os.path.join(save_name.split('.')[0], '0000{}.jpg'.format(str(i))), img=frame)