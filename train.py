a407a1fba17383a97caa67f87116ff5391f004cc
leftthomas <leftthomas@qq.com>
2019-04-11 16:09:39 +0800

    use the best optima config

--- train.py
+++ train.py
@@ -158,7 +158,7 @@ if __name__ == '__main__':
         else:
             raise ValueError("the machine don't have {} gpus".format(str(len(device_ids))))
     loss_criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(params=model.parameters(), lr=1e-5)
+    optimizer = optim.Adam(params=model.parameters(), lr=1e-4, weight_decay=5e-4)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, verbose=True)
     print("Number of parameters:", sum(param.numel() for param in model.parameters()))
 
