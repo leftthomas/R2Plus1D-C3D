73687bdc694c196470a786dc247370c828185ed2
leftthomas <leftthomas@qq.com>
2019-04-12 11:27:20 +0800

    fast

--- download.py
+++ download.py
@@ -109,6 +109,8 @@ def download_clip(video_identifier, output_filename, start_time, end_time, url_b
 
 def download_clip_wrapper(row, label_to_dir, trim_format):
     output_filename = construct_video_filename(row, label_to_dir, trim_format)
+    if os.path.exists(output_filename):
+        return 'youtube video {} have already been saved to {}'.format(row['video-id'], output_filename)
     status = download_clip(row['video-id'], output_filename, row['start-time'], row['end-time'])
     return status
 
